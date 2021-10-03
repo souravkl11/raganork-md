5d59193a781840dc834bb1830ed4a781a5e2ad20
SKL!! <87891508+souravkl11@users.noreply.github.com>
2021-10-03 10:15:16 +0530

    FIXED ERRORS

--- plugins/alive.js
+++ plugins/alive.js
@@ -26,7 +26,7 @@ if (Config.WORKTYPE == 'private') {
         );
     }));
 }
-else if (Config.ALIVE == 'public') {
+else if (Config.WORKTYPE == 'public') {
 
    Asena.addCommand({pattern: 'alive', fromMe: false, desc: Lang.ALIVE_DESC}, (async (message, match) => {
         
@@ -34,9 +34,16 @@ else if (Config.ALIVE == 'public') {
         try { pp = await message.client.getProfilePicture(message.jid.includes('-') ? message.data.participant : message.jid ); } catch { pp = await message.client.getProfilePicture(); }
         await axios.get(Config.LOGOSK, {responseType: 'arraybuffer'}).then(async (res) => { await message.client.sendMessage(message.jid, res.data, MessageType.image, { caption: Config.ALIVEMSG }); });
     }));
-}
 
-Asena.addCommand({pattern: 'sysd', fromMe: false, desc: Lang.SYSD_DESC}, (async (message, match) => {
+    Asena.addCommand({pattern: 'sysd', fromMe: false, desc: Lang.SYSD_DESC}, (async (message, match) => {
+
+        const child = spawnSync('neofetch', ['--stdout']).stdout.toString('utf-8')
+        await message.sendMessage(
+            '```' + child + '```', MessageType.text
+        );
+    }));
+    
+    Asena.addCommand({pattern: 'psysd', fromMe: true, desc: Lang.SYSD_DESC, dontAddCommandList: true }, (async (message, match) => {
 
         const child = spawnSync('neofetch', ['--stdout']).stdout.toString('utf-8')
         await message.sendMessage(
