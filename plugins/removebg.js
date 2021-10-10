c679817e2b1d36b73ed9bc10004cff57bceaa5fb
SKL!! <87891508+souravkl11@users.noreply.github.com>
2021-10-10 11:49:37 +0530

    👀

--- plugins/removebg.js
+++ plugins/removebg.js
@@ -25,7 +25,7 @@ if (Config.WORKTYPE == 'private') {
     Asena.addCommand({pattern: 'removebg ?(.*)', fromMe: true, desc: Lang.REMOVEBG_DESC}, (async (message, match) => {    
 
         if (message.reply_message === false || message.reply_message.image === false) return await message.client.sendMessage(message.jid,Lang.NEED_PHOTO,MessageType.text);
-        if (Config.RBG_API_KEY === false) return await message.client.sendMessage(message.jid,Lang.NO_API_KEY.replace('remove.bg', 'https://github.com/phaticusthiccy/WhatsAsenaDuplicated/wiki/Remove-BG-API-Key'),MessageType.text);
+        if (Config.RBG_API_KEY === false) return await message.client.sendMessage(message.jid,Lang.NO_API_KEY.replace('remove.bg', 'Get an api key now'),MessageType.text);
     
         var load = await message.reply(Lang.RBGING);
         var location = await message.client.downloadAndSaveMediaMessage({
