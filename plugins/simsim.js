00877686e0a6d1245f9fa3eef23f8bc684ebd6bc
SKL!! <87891508+souravkl11@users.noreply.github.com>
2021-10-16 08:36:44 +0530

    SIMI SIMI

--- plugins/simsim.js
+++ plugins/simsim.js
@@ -16,7 +16,7 @@ if (Config.WORKTYPE == 'private') {
 
     Asena.addCommand({pattern: 'bot ?(.*)', fromMe: true, desc: Lang.BOT_DESC}, async (message, match) => {
 	if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
-	const url = `https://api.simsimi.net/v1/?text=${match[1]}&lang=en&cf=true`;
+	const url = `https://api.simsimi.net/v2/?text=${match[1]}&lang=en&cf=true`;
 	try {
 		const response = await got(url);
 		const json = JSON.parse(response.body);
@@ -30,7 +30,7 @@ else if (Config.WORKTYPE == 'public') {
 
     Asena.addCommand({pattern: 'bot ?(.*)', fromMe: false, desc: Lang.BOT_DESC}, async (message, match) => {
 	if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
-	const url = `https://api.simsimi.net/v1/?text=${match[1]}&lang=en&cf=true`;
+	const url = `https://api.simsimi.net/v2/?text=${match[1]}&lang=en&cf=true`;
 	try {
 		const response = await got(url);
 		const json = JSON.parse(response.body);
