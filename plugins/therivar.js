019b81138ea00e16eb5618639f5a3f26313815c7
SKL!! <87891508+souravkl11@users.noreply.github.com>
2021-10-17 11:29:54 +0530

    #1

--- plugins/therivar.js
+++ plugins/therivar.js
@@ -1,9 +1,3 @@
-/* Copyright (C) 2020 Yusuf Usta.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-WhatsAsena - Yusuf Usta
-*/
-
 const Asena = require('../events');
 const config = require('../config');
 const Heroku = require('heroku-client');
@@ -22,8 +16,8 @@ let baseURI = '/apps/' + config.HEROKU.APP_NAME;
         l_dsc = 'chilla theri vilichaal spot kick. -bot owner command'
         alr_on = 'Antilink is already open!'
         alr_off = 'Antilink is currently closed!'
-        THERI_on = '*eni chilla theri vilcha kick akkum*'
-        THERI_off = '*eni enth theri vennelum vili*'
+        THERI_on = '*Anti theri mode activated. May kick when using some bad words*'
+        THERI_off = '*Anti theri mode deactivated. Theri vilicho no problem*'
     }
    
     if (config.LANG == 'HI') {
@@ -37,19 +31,19 @@ let baseURI = '/apps/' + config.HEROKU.APP_NAME;
         l_dsc = 'ആന്റിലിങ്ക് ഉപകരണം സജീവമാക്കുന്നു.'
         alr_on = 'ആന്റിലിങ്ക് ഇതിനകം തുറന്നു!'
         alr_off = 'ആന്റിലിങ്ക് നിലവിൽ അടച്ചിരിക്കുന്നു!'
-        THERI_on = 'bgm option turned on'
-        THERI_off = 'bgm option turned off'
+        THERI_on = 'ഇനി തെറി വിളിച്ചാൽ ഗ്രൂപ്പിൽ നിന്ന് പുറത്താക്കും. ബോട്ട് പുനരാരംഭിക്കുന്നു'
+        THERI_off = 'ഇനി ഗ്രൂപ്പിൽ എന്ത് തെറി വേണെങ്കിലും വിളിക്കാം'
     }
    
-    Asena.addCommand({pattern: 'theri ?(.*)', fromMe: true, desc: l_dsc, usage: '.theri no / yes' }, (async (message, match) => {
-        if (match[1] == 'yes') {
+    Asena.addCommand({pattern: 'therikick ?(.*)', fromMe: true, desc: l_dsc, usage: '.theri no / yes' }, (async (message, match) => {
+        if (match[1] == 'off') {
                 await heroku.patch(baseURI + '/config-vars', { 
                     body: { 
                         ['THERI_KICK']: 'false'
                     } 
                 });
                 await message.sendMessage(THERI_off)
-        } else if (match[1] == 'no') {
+        } else if (match[1] == 'on') {
                 await heroku.patch(baseURI + '/config-vars', { 
                     body: { 
                         ['THERI_KICK']: 'true'
