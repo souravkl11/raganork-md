4562dce7ec6e3c106b80a964525a1d00fb7962b8
SKL!! <87891508+souravkl11@users.noreply.github.com>
2021-10-30 14:39:07 +0530

    URL FIX

--- plugins/url.js
+++ plugins/url.js
@@ -3,7 +3,7 @@ const {MessageType} = require('@adiwajshing/baileys');
 const Config = require('../config');
 
 let sk = Config.WORKTYPE == 'public' ? false : true
-skl.addCommand({pattern: 'del ?(.*)', fromMe: sk}, async (message, match) => {
+skl.addCommand({pattern: 'url ?(.*)', fromMe: sk}, async (message, match) => {
 
     await message.sendMessage('Use this site to upload images and get URL 👇:\n\n https://souravkl11.github.io/image-uploader/')
 
