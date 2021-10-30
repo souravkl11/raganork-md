c9b39ea0d3505511862614937ba79e9960a2e179
SKL!! <87891508+souravkl11@users.noreply.github.com>
2021-10-30 15:05:18 +0530

    FIXING PRIVATE MODE FOR CLEANER EXPERIENCE 🛠

--- plugins/weather.js
+++ plugins/weather.js
@@ -15,29 +15,9 @@ const Config = require('../config');
 const Language = require('../language');
 const Lang = Language.getString('weather');
 
-if (Config.WORKTYPE == 'private') {
+let sk = Config.WORKTYPE == 'public' ? false : true
 
-    Asena.addCommand({pattern: 'weather ?(.*)', desc: Lang.WEATHER_DESC, fromMe: true}, async (message, match) => {
-
-	    if (match[1] === '') return await message.reply(Lang.NEED_LOCATION);
-	    const url = `http://api.openweathermap.org/data/2.5/weather?q=${match[1]}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273&language=tr`;
-	    try {
-		    const response = await got(url);
-		    const json = JSON.parse(response.body);
-		    if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '*📍 ' + Lang.LOCATION +':* ```' + match[1] + '```\n\n' +
-		    '*☀ ' + Lang.TEMP +':* ```' + json.main.temp_max + '°```\n' + 
-		    '*ℹ ' + Lang.DESC +':* ```' + json.weather[0].description + '```\n' +
-		    '*☀ ' + Lang.HUMI +':* ```%' + json.main.humidity + '```\n' + 
-		    '*💨 ' + Lang.WIND +':* ```' + json.wind.speed + 'm/s```\n' + 
-		    '*☁ ' + Lang.CLOUD +':* ```%' + json.clouds.all + '```\n', MessageType.text);
-	    } catch {
-		    return await message.client.sendMessage(message.jid, Lang.NOT_FOUND, MessageType.text);
-	    }
-    });
-}
-if (Config.WORKTYPE == 'public') {
-
-    Asena.addCommand({pattern: 'weather ?(.*)', desc: Lang.WEATHER_DESC, fromMe: false}, async (message, match) => {
+    Asena.addCommand({pattern: 'weather ?(.*)', desc: Lang.WEATHER_DESC, fromMe: sk}, async (message, match) => {
 
 	    if (match[1] === '') return await message.reply(Lang.NEED_LOCATION);
 	    const url = `http://api.openweathermap.org/data/2.5/weather?q=${match[1]}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273&language=tr`;
@@ -54,21 +34,4 @@ if (Config.WORKTYPE == 'public') {
 		    return await message.client.sendMessage(message.jid, Lang.NOT_FOUND, MessageType.text);
 	    }
     });
-    Asena.addCommand({pattern: 'weather ?(.*)', desc: Lang.WEATHER_DESC, fromMe: true, dontAddCommandList: true}, async (message, match) => {
 
-	    if (match[1] === '') return await message.reply(Lang.NEED_LOCATION);
-	    const url = `http://api.openweathermap.org/data/2.5/weather?q=${match[1]}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273&language=tr`;
-	    try {
-		    const response = await got(url);
-		    const json = JSON.parse(response.body);
-		    if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '*📍 ' + Lang.LOCATION +':* ```' + match[1] + '```\n\n' +
-		    '*☀ ' + Lang.TEMP +':* ```' + json.main.temp_max + '°```\n' + 
-		    '*ℹ ' + Lang.DESC +':* ```' + json.weather[0].description + '```\n' +
-		    '*☀ ' + Lang.HUMI +':* ```%' + json.main.humidity + '```\n' + 
-		    '*💨 ' + Lang.WIND +':* ```' + json.wind.speed + 'm/s```\n' + 
-		    '*☁ ' + Lang.CLOUD +':* ```%' + json.clouds.all + '```\n', MessageType.text);
-	    } catch {
-		    return await message.client.sendMessage(message.jid, Lang.NOT_FOUND, MessageType.text);
-	    }
-    });
-}
