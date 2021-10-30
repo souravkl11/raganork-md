f2225c8371fd338bfd35843859750054d41502c3
Romeo <90444506+ROMEO-7@users.noreply.github.com>
2021-10-30 06:46:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBU
 module.exports = {
     VERSION: 'v3.0.0',
     CHANNEL: 'https://t.me/remasterplugin',
-    SESSION: process.env.RAGANORK_CODE === undefined ? '' : process.env.RAGANORK_CODE,
+    SESSION:  === undefined ? '' : process.env.RAGANORK_CODE,
     ANTİLİNK: process.env.ANTİ_LİNK === undefined ? 'false' : process.env.ANTİ_LİNK,
     AFN: process.env.ALL_CAPTION === undefined ? '*MADE BY YOUR BOT NAME*' : process.env.ALL_CAPTION,
     AUTOBİO: process.env.AUTO_BİO === undefined ? 'false' : process.env.AUTO_BİO,
