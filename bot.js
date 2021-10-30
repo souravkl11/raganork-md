3fbc024611a73384ad2550cf1eabd03f06c17832
SKL!! <87891508+souravkl11@users.noreply.github.com>
2021-10-30 10:10:26 +0530

    ```EHH 👀```

--- bot.js
+++ bot.js
@@ -18,6 +18,10 @@ const git = simpleGit();
 const axios = require('axios');
 const got = require('got');
 
+const Language = require('./language');
+const Lang = Language.getString('updater');
+
+
 // Sql
 const WhatsAsenaDB = config.DATABASE.define('WhatsAsena', {
     info: {
