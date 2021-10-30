af34f267664b712375876acc80243b406e1ab9ab
SKL!! <87891508+souravkl11@users.noreply.github.com>
2021-10-30 15:07:27 +0530

    FIXING PRIVATE MODE FOR CLEANER EXPERIENCE 🛠

--- plugins/neko.js
+++ plugins/neko.js
@@ -14,29 +14,9 @@ const Config = require('../config');
 
 const Language = require('../language');
 const Lang = Language.getString('nekobin');
+let sk = Config.WORKTYPE == 'public' ? false : true
 
-if (Config.WORKTYPE == 'private') {
-
-    Asena.addCommand({pattern: 'neko', fromMe: true, desc: Lang.NEKO_DESC}, (async (message, match) => {
-
-        if (!message.reply_message) return await message.sendMessage(Lang.NEED_REPLY);
-        if (!message.reply_message.text) return await message.sendMessage(Lang.MUST_TEXT);
-        let base_URI = "https://nekobin.com/api/documents";
-        try {
-            const response = await got.post(base_URI, {json : {content : message.reply_message.text}}); 
-            json = JSON.parse(response.body);
-            neko_url = 'https://nekobin.com/' + json.result.key;
-            await message.reply(neko_url);        
-        } catch (err) {
-            await message.reply(err.message, MessageType.text);
-            console.log(err.message);
-        }
-       
-    }));
-}
-else if (Config.WORKTYPE == 'public') {
-
-    Asena.addCommand({pattern: 'neko', fromMe: false, desc: Lang.NEKO_DESC}, (async (message, match) => {
+ Asena.addCommand({pattern: 'neko', fromMe: sk, desc: Lang.NEKO_DESC}, (async (message, match) => {
 
         if (!message.reply_message) return await message.sendMessage(Lang.NEED_REPLY);
         if (!message.reply_message.text) return await message.sendMessage(Lang.MUST_TEXT);
@@ -52,20 +32,3 @@ else if (Config.WORKTYPE == 'public') {
         }
        
     }));
-    Asena.addCommand({pattern: 'neko', fromMe: true, desc: Lang.NEKO_DESC, dontAddCommandList: true}, (async (message, match) => {
-
-        if (!message.reply_message) return await message.sendMessage(Lang.NEED_REPLY);
-        if (!message.reply_message.text) return await message.sendMessage(Lang.MUST_TEXT);
-        let base_URI = "https://nekobin.com/api/documents";
-        try {
-            const response = await got.post(base_URI, {json : {content : message.reply_message.text}}); 
-            json = JSON.parse(response.body);
-            neko_url = 'https://nekobin.com/' + json.result.key;
-            await message.reply(neko_url);        
-        } catch (err) {
-            await message.reply(err.message, MessageType.text);
-            console.log(err.message);
-        }
-       
-    }));
-}
