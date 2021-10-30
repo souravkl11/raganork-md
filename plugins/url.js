b30cc870333c9efa36534425ada93d3efa895041
SKL!! <87891508+souravkl11@users.noreply.github.com>
2021-10-30 10:02:30 +0530

    ```EH 👀```

--- plugins/url.js
+++ plugins/url.js
@@ -1,5 +1,6 @@
 const skl = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
+const Config = require('../config');
 
 let sk = Config.WORKTYPE == 'public' ? false : true
 skl.addCommand({pattern: 'del ?(.*)', fromMe: sk}, async (message, match) => {
