5898731196187b54104787627cd074727033c78c
SKL!! <87891508+souravkl11@users.noreply.github.com>
2021-11-05 18:02:06 +0530

    TRYING NEW SOND DOWNLOADER ⏫

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -426,7 +426,7 @@ if (config.WORKTYPE == 'private') {
                 return;
     
             let 
-                LANG = config.LANG,
+                LANG = conf.LANG.toLowerCase(),
                 ttsMessage = match[1],
                 SPEED = 1.0
 
@@ -449,34 +449,15 @@ if (config.WORKTYPE == 'private') {
     skl.addCommand({pattern: 'song ?(.*)', fromMe: true, desc: Lang.SONG_DESC}, (async (message, match) => { 
 
         if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_TEXT_SONG,MessageType.text);    
-        let arama = await yts(match[1]);
-        arama = arama.all;
-        if(arama.length < 1) return await message.client.sendMessage(message.jid,Lang.NO_RESULT,MessageType.text);
-        var reply = await message.client.sendMessage(message.jid,Lang.DOWNLOADING_SONG,MessageType.text);
+        await message.client.sendMessage(message.jid,Lang.DOWNLOADING_SONG,MessageType.text);
 
-        let title = arama[0].title.replace(' ', '+');
-        let stream = ytdl(arama[0].videoId, {
-            quality: 'highestaudio',
-        });
-    
-        got.stream(arama[0].image).pipe(fs.createWriteStream(title + '.jpg'));
-        ffmpeg(stream)
-            .audioBitrate(320)
-            .save('./' + title + '.mp3')
-            .on('end', async () => {
-                const writer = new ID3Writer(fs.readFileSync('./' + title + '.mp3'));
-                writer.setFrame('TIT2', arama[0].title)
-                    .setFrame('TPE1', [arama[0].author.name])
-                    .setFrame('APIC', {
-                        type: 3,
-                        data: fs.readFileSync(title + '.jpg'),
-                        description: arama[0].description
-                    });
-                writer.addTag();
-
-                reply = await message.client.sendMessage(message.jid,Lang.UPLOADING_SONG,MessageType.text);
-                await message.client.sendMessage(message.jid,Buffer.from(writer.arrayBuffer), MessageType.audio, {mimetype: Mimetype.mp4Audio, quoted: message.data, ptt: false});
-            });
+        
+		const {data} = await axios(`https://zenzapi.xyz/api/play/playmp3?query=(match[1])&apikey=souravkl11`)
+        const { result } = data
+        if (match[1] === '') return await message.sendMessage('_Type a song name_');
+        var souravkl11 = await axios.get(`${result.url}`, { responseType: 'arraybuffer' })
+        await message.sendMessage(Buffer.from(souravkl11.data), MessageType.audio, { mimetype: Mimetype.mp3})
+     
     }));
     skl.addCommand({pattern: 'video ?(.*)', fromMe: true, desc: Lang.VIDEO_DESC}, (async (message, match) => { 
 
