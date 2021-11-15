d173d3853018302b64deb340ce8ef3dc62ee891c
SKL!! <87891508+souravkl11@users.noreply.github.com>
2021-11-15 11:10:38 +0530

    `USER CAN ADD BGM! Coming soon...`

---
+++ plugins/sql/BgmDB.js
@@ -0,0 +1,34 @@
+const config = require('../../config');
+const { DataTypes } = require('sequelize');
+
+const BgmDB = config.DATABASE.define('bgm', {
+    note: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    }
+});
+
+
+async function getBgm() {
+    const Notes = await NotesDB.findAll()
+
+    return Notes
+}
+
+async function addBgm(bgm) {
+    return await BgmDB.create({ bgm });
+}
+
+async function deleteBgm() {
+    return await BgmDB.destroy({
+        where: {},
+        truncate: true
+    })
+}
+
+module.exports = {
+    BgmDB,
+    getBgm,
+    addBGM,
+    deleteBgm
+};
