3ba9cf98a7517713b9ad89f090413977ea29176a
SKL!! <87891508+souravkl11@users.noreply.github.com>
2021-12-11 19:48:40 +0530

    fix typo

--- plugins/insta.js
+++ plugins/insta.js
@@ -13,6 +13,6 @@ skl.addCommand({ pattern: 'insta ?(.*)', fromMe: sourav,dontAddCommandList: true
     if (!query[1].includes('instagram.com')) return await msg.client.sendMessage(msg.jid, need, MessageType.text, {quoted: msg.data});
     var res = await post.getInsta(query[1])
     var buffer = await get.skbuffer(res.links[0].url)
-    if (res.links[0].url.includes('mp4')) return await msg.client.sendMessage(msg.jid, buffer, MessageType.video, { mimetype: Mimetype.mp4, caption: '_Caption:_ ' + `${res.caption}` + '\n _Username:_ *' + `${res.username}` + '\n _Likes:_ *' + `${res.likes}` + '\n _Comments:_ *' + `${res.comment_count}`, quoted: msg.data});
-    if (res.links[0].url.includes('jpg')) return await msg.client.sendMessage(msg.jid, buffer, MessageType.image, { mimetype: Mimetype.jpg, caption: '_Caption:_ ' + `${res.caption}` + '\n _Username:_ *' + `${res.username}` + '\n _Likes:_ *' + `${res.likes}` + '\n _Comments:_ *' + `${res.comment_count}`, quoted: msg.data});
+    if (res.links[0].url.includes('mp4')) return await msg.client.sendMessage(msg.jid, buffer, MessageType.video, { mimetype: Mimetype.mp4, caption: '_Caption:_ ' + `${res.caption}` + '\n\n _Username:_ *' + `${res.username}` + '*\n _Name:_ *' + `${res.name}` + '*\n _Likes:_ *' + `${res.likes}` + '*\n _Comments:_ *' + `${res.comment_count}` + '*', quoted: msg.data});
+    if (res.links[0].url.includes('jpg')) return await msg.client.sendMessage(msg.jid, buffer, MessageType.image, { mimetype: Mimetype.jpg, caption: '_Caption:_ ' + `${res.caption}` + '\n\n _Username:_ *' + `${res.username}` + '*\n _Name:_ *' + `${res.name}` + '*\n _Likes:_ *' + `${res.likes}` + '*\n _Comments:_ *' + `${res.comment_count}` + '*', quoted: msg.data});
     }));
