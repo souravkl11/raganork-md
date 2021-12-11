eccdf85577f46f5d2ff47fa4cabda4bcc027a9c2
SKL!! <87891508+souravkl11@users.noreply.github.com>
2021-12-11 18:10:05 +0530

    fix insta test

--- plugins/insta.js
+++ plugins/insta.js
@@ -1,91 +1,18 @@
-// (c) souravkl11/raganork 2021
-(function (k, u) {
-    function j(k, u) {
-        return e(u - -'0x5e', k);
-    }
-    const c = k();
-    while (!![]) {
-        try {
-            const G = -parseInt(j('0xbf', '0xc5')) / (-0xe * 0x80 + 0x23b1 + -0x1cb0) * (-parseInt(j('0xc5', '0xbe')) / (-0x1513 + 0xacc * -0x1 + 0x1fe1)) + parseInt(j(0xe0, '0xc7')) / (0x122b * 0x1 + 0x65 * 0x2f + -0x5 * 0x757) + parseInt(j(0x9a, '0xaf')) / (0x2674 + 0x94c + -0x2 * 0x17de) * (-parseInt(j(0xd4, 0xbb)) / (-0x136 + -0xa35 + 0xb70)) + -parseInt(j('0xcc', '0xbd')) / (0x57b * 0x7 + 0x19e0 + -0x4037) + parseInt(j('0x7d', '0x93')) / (-0x1be7 * -0x1 + -0x67f + -0x1561) + -parseInt(j('0xa8', '0xac')) / (-0xd * 0x217 + 0x663 + -0x14d * -0x10) * (-parseInt(j('0x98', '0xa6')) / (-0x1 * 0x1c6e + 0xbcb + 0x10ac)) + -parseInt(j('0xa9', 0x8d)) / (-0x2 * 0x187 + 0x3 * 0xa82 + -0xe37 * 0x2);
-            if (G === u) break;
-            else c['push'](c['shift']());
-        } catch (o) {
-            c['push'](c['shift']());
-        }
-    }
-}(x, 0x2 * 0x161f1 + 0x49a13 + -0xd * 0x5864));
-const skl = require(y('0x4a3', '0x486')),
-    {
-        MessageType,
-        MessageOptions,
-        Mimetype
-    } = require(y('0x47b', 0x493) + y(0x47e, 0x465)),
-    axios = require(y(0x478, 0x48c)),
-    instagram = require(y(0x487, '0x471') + y('0x492', '0x496') + 'er'),
-    Config = require(y('0x46b', 0x460));
-
-function y(k, u) {
-    return e(k - '0x382', u);
-}
-
-function x() {
-    const l = ['688524GskQjx', '2BYZEFy', 'url', 'insta ?(.*', 'vkl11/d891', 'video', '../events', 'text', '32767vJHFtN', '\x0a _Comment', '819984xUmErN', '../config', 'jid', '4033750ZJwzoG', '\x0a _Likes:_', 'setCookie', 'then', 'nstagram p', 'reel', '2170476fIMPpW', 'sercontent', 'includes', 'st.githubu', 'com', 'axios', 'WORKTYPE', 'comment_co', '@adiwajshi', 'inclused', '_Caption:_', 'ng/baileys', 'mp4', '_Download ', 'client', 'fromMe', 'https://gi', 'caption', 'e:_ *', '525249LebIMm', 'raganork-i', 'mandList', 's:_ *', 'unt', '\x0a _Usernam', '16PnDNMD', 'responseTy', 'f3b3e635/r', '4XHITNO', 'links', 'get', 'nsta-scrap', 'instagram.', 'from', 'likes', 'sendMessag', 'dontAddCom', '_Need an i', 'pattern', 'data', '130575ccUOtU', '69387eab15'];
-    x = function () {
-        return l;
-    };
-    return x();
-}
-const need = y('0x498', 0x4a7) + y(0x471, 0x479) + 'ost/reel l' + 'ink!_';
-let sourav = Config[y(0x479, '0x48f')] == 'public' ? ![] : !![];
-const I = {};
-
-function e(I, k) {
-    const u = x();
-    return e = function (c, G) {
-        c = c - (0x1 * 0x4ed + 0x103e + -0x2 * 0xa21);
-        let o = u[c];
-        return o;
-    }, e(I, k);
-}
-I[y(0x499, '0x4b0')] = y(0x4a0, '0x485') + ')', I[y('0x482', '0x482')] = sourav, I[y('0x497', '0x484') + y('0x488', '0x46d')] = !![], skl['addCommand'](I, async (k, u) => {
-    if (!u[0x18ab * -0x1 + 0x38f * 0x9 + -0x75b]) return await k[A(-'0xa2', -0xba)][A(-0x92, -0xa5) + 'e'](k[A(-0xb6, -'0xcf')], need, MessageType[A(-'0xa7', -0x97)], {
-        'quoted': k['data']
-    });
-    if (!u[-0x17a3 + -0x30 * 0xb5 + 0x3994][A(-0xbe, -0xc6)](A(-0xb9, -'0xa8') + A(-0xd2, -'0xc4'))) return await k[A(-0xd3, -0xba)][A(-0x92, -0xa5) + 'e'](k['jid'], need, MessageType['text'], {
-        'quoted': k[A(-0x90, -'0xa1')]
-    });
-    const {
-        data: c
-    } = await axios(A(-'0xb8', -'0xb8') + A(-0xb6, -0xc5) + A(-'0xd4', -'0xc7') + '.com/soura' + A(-0xb7, -0x9a) + '6efc8889b7' + A(-'0xaf', -0x9f) + A(-'0x90', -'0xad') + 'aw');
-
-    function A(k, u) {
-        return y(u - -'0x53b', k);
-    }
-    const {
-        session: G
-    } = c;
-    instagram[A(-0xe4, -'0xcc')](G), instagram['fetchPost'](u[-0x21f3 * -0x1 + -0x2 * -0x1ff + -0x1 * 0x25f0]['replace'](A(-'0xc2', -'0xc9'), 'p'))[A(-0xda, -0xcb)](async o => {
-        function g(k, u) {
-            return A(k, u - '0x1e4');
-        }
-        if (o) {
-            const H = {};
-            H[g('0x145', 0x136) + 'pe'] = 'arraybuffe' + 'r';
-            var J = await axios[g(0x14c, '0x13a')](o[g(0x11e, '0x139')][-0x19f9 + -0x1da5 + 0x15 * 0x2a6][g('0x130', 0x148)], H),
-                D = Buffer[g(0x127, 0x13d)](J[g(0x148, 0x143)]);
-            if (o[g('0x120', 0x139)][-0xa42 + 0xd * -0x157 + 0x41 * 0x6d][g(0x156, 0x148)][g('0x10a', '0x11e')](g('0x12a', '0x128'))) return await k[g('0x121', 0x12a)]['sendMessag' + 'e'](k['jid'], D, MessageType[g(0x132, 0x14b)], {
-                'mimetype': Mimetype[g(0x10b, '0x128')],
-                'caption': g('0x120', '0x126') + ' ' + ('' + o[g(0x142, '0x12d')]) + (g(0x12b, 0x134) + g('0x123', '0x12e')) + ('' + o['username']) + (g('0x104', 0x117) + ' *') + ('' + o['likes']) + (g(0x167, 0x14f) + g(0x118, '0x132')) + ('' + o[g(0x11c, '0x123') + g(0x123, '0x133')]),
-                'quoted': k['data']
-            });
-            if (o['links'][0x2 * -0xd6e + -0x14 * 0x97 + 0x2 * 0x1354][g(0x13c, '0x148')][g('0x10c', 0x125)]('jpg')) return await k[g(0x117, '0x12a')]['sendMessag' + 'e'](k['jid'], D, MessageType['image'], {
-                'mimetype': Mimetype['jpg'],
-                'caption': g(0x145, '0x126') + ' ' + ('' + o['caption']) + (g('0x129', 0x134) + g(0x11f, 0x12e)) + ('' + o['username']) + (g(0x12e, 0x117) + ' *') + ('' + o[g('0x122', 0x13e)]) + (g('0x13f', '0x14f') + g('0x144', '0x132')) + ('' + o['comment_co' + 'unt']),
-                'quoted': k[g(0x12d, '0x143')],
-                'thumbnail': null
-            });
-        } else return await k[g('0x11a', '0x12a')]['sendMessag' + 'e'](k[g(0x124, '0x115')], g(0x12d, '0x129') + 'failed!_', MessageType[g('0x16a', '0x14d')], {
-            'quoted': k[g('0x141', '0x143')]
-        });
-    });
-});
+const skl = require('../events');
+const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
+const fs = require('fs');
+const axios = require('axios');
+const setting = require('../config');
+const get = require('../buffer');
+const post = require('../insta_srap');
+const Config = require('../config');
+const need = "_Need instagram link!_*";
+let sourav = setting.WORKTYPE == 'public' ? false : true
+skl.addCommand({ pattern: 'insta ?(.*)', fromMe: sourav,dontAddCommandList: true }, (async (msg, query) => {
+    if (query[1] === '') return await msg.client.sendMessage(msg.jid, need, MessageType.text, {quoted: msg.data});
+    if (!query[1].includes('instagram.com')) return await msg.client.sendMessage(msg.jid, need, MessageType.text, {quoted: msg.data});
+    var res = await post.getInsta(query[1])
+    var buffer = await get.skbuffer(res.links[0].url)
+    if (res.links[0].url.includes('mp4')) return await msg.client.sendMessage(msg.jid, buffer, MessageType.video, { mimetype: Mimetype.mp4, caption: '_Caption:_ ' + `${res.caption}` + '\n _Username:_ *' + `${res.username}` + '\n _Likes:_ *' + `${res.likes}` + '\n _Comments:_ *' + `${res.comment_count}`, quoted: msg.data});
+    if (res.links[0].url.includes('jpg')) return await msg.client.sendMessage(msg.jid, buffer, MessageType.image, { mimetype: Mimetype.jpg, caption: '_Caption:_ ' + `${res.caption}` + '\n _Username:_ *' + `${res.username}` + '\n _Likes:_ *' + `${res.likes}` + '\n _Comments:_ *' + `${res.comment_count}`, quoted: msg.data});
+    }));
