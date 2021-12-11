f6593f053dffcdad6f297ae92343eba759a518e2
SKL!! <87891508+souravkl11@users.noreply.github.com>
2021-12-11 15:39:32 +0530

    fix delete

--- plugins/delete.js
+++ plugins/delete.js
@@ -3,5 +3,5 @@ const {MessageType} = require('@adiwajshing/baileys');
 const DEL_DESC = "Deletes replied message! (owner command)"
 
 New.addCommand({pattern: 'del', fromMe: true, desc: DEL_DESC}, (async (multidevice, match) => {
-await multidevice.deleteMessage(multidevice.jid, {id: multidevice.reply_message.id, remoteJid: multidevice.reply_message.jid, fromMe: true})
+await multidevice.client.deleteMessage(multidevice.jid, {id: multidevice.reply_message.id, remoteJid: multidevice.reply_message.jid, fromMe: true})
 }));
