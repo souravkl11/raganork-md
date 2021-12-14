c3d797e41b4155f5427c20bef5ad72314d40d0a5
Sourav K <87891508+souravkl11@users.noreply.github.com>
2021-12-14 19:06:10 +0530

    fix

--- plugins/insta.js
+++ plugins/insta.js
@@ -6,8 +6,8 @@ const setting = require('../config');
 const get = require('../buffer');
 const insta = require('../insta_scrap');
 const Config = require('../config');
-const need = "_Need instagram link!_*";
-const need_acc = "_Need an instagram username!_*";
+const need = "*_Need instagram link!_*";
+const need_acc = "*_Need an instagram username!_*";
 let sourav = setting.WORKTYPE == 'public' ? false : true
 skl.addCommand({ pattern: 'insta ?(.*)', fromMe: sourav,dontAddCommandList: true }, (async (msg, query) => {
     if (query[1] === '') return await msg.client.sendMessage(msg.jid, need, MessageType.text, {quoted: msg.data});
@@ -21,5 +21,5 @@ skl.addCommand({ pattern: 'ig ?(.*)', fromMe: sourav,dontAddCommandList: true },
     if (query[1] === '') return await msg.client.sendMessage(msg.jid, need_acc, MessageType.text, {quoted: msg.data});
     var res = await insta.getStalk(query[1])
     var buffer = await get.skbuffer(res.user.profile_pic_url)
-    await msg.client.sendMessage(msg.jid, buffer, MessageType.image, { mimetype: Mimetype.jpg, caption: '_Name:_ ' + `${res.user.full_name}` + '\n _Bio:_ *' + `${res.user.biography}`+ '*\n _Private acc:_ *' + `${res.user.is_private} ` + '\n _Followers:_ *' + `${res.user.follower_count}` + '*\n _Following:_ *' + `${res.user.following_count}` + '*\n _Posts:_ *' + `${res.user.media_count}` + '*\n _Verified:_ *' + `${res.user.is_verified} ` + '*\n _IGTV videos:_ *' + `${res.user.total_igtv_videos} ` + '*', quoted: msg.data});
+    await msg.client.sendMessage(msg.jid, buffer, MessageType.image, { mimetype: Mimetype.jpg, caption: '_Name:_ ' + `${res.user.full_name}` + '\n _Bio:_ *' + `${res.user.biography}`+ '*\n _Private acc:_ *' + `${res.user.is_private} ` + '\n _Followers:_ *' + `${res.user.follower_count}` + '*\n _Following:_ *' + `${res.user.following_count}` + '*\n _Posts:_ *' + `${res.user.media_count}` + '*\n _Verified:_ *' + `${res.user.is_verified} ` + '*\n _IGTV videos:_ *' + `${res.user.total_igtv_videos}` + '*', quoted: msg.data});
     }));
