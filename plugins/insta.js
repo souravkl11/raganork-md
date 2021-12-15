0b6de0f59e834d0c9d5cae4ee27160d9a94ff9a7
Sourav K <87891508+souravkl11@users.noreply.github.com>
2021-12-15 20:50:04 +0530

    Added story downloader!

--- plugins/insta.js
+++ plugins/insta.js
@@ -8,6 +8,7 @@ const insta = require('../insta_scrap');
 const Config = require('../config');
 const need = "*_Need instagram link!_*";
 const need_acc = "*_Need an instagram username!_*";
+const need_acc_s = "*_Need an instagram username and story count!_* \n ```Example: \n .story dqsalmaan/4```";
 let sourav = setting.WORKTYPE == 'public' ? false : true
 skl.addCommand({ pattern: 'insta ?(.*)', fromMe: sourav,dontAddCommandList: true }, (async (msg, query) => {
     if (query[1] === '') return await msg.client.sendMessage(msg.jid, need, MessageType.text, {quoted: msg.data});
@@ -22,4 +23,16 @@ skl.addCommand({ pattern: 'ig ?(.*)', fromMe: sourav,dontAddCommandList: true },
     var res = await insta.getStalk(query[1])
     var buffer = await get.skbuffer(res.user.profile_pic_url)
     await msg.client.sendMessage(msg.jid, buffer, MessageType.image, { mimetype: Mimetype.jpg, caption: '_Name:_ ' + `${res.user.full_name}` + '\n _Bio:_ *' + `${res.user.biography}`+ '*\n _Private acc:_ *' + `${res.user.is_private} ` + '\n _Followers:_ *' + `${res.user.follower_count}` + '*\n _Following:_ *' + `${res.user.following_count}` + '*\n _Posts:_ *' + `${res.user.media_count}` + '*\n _Verified:_ *' + `${res.user.is_verified} ` + '*\n _IGTV videos:_ *' + `${res.user.total_igtv_videos}` + '*', quoted: msg.data});
+    }));
+skl.addCommand({ pattern: 'story ?(.*)', fromMe: sourav,dontAddCommandList: true }, (async (msg, query) => {
+    if (query[1] === '') return await msg.client.sendMessage(msg.jid, need_acc_s, MessageType.text, {quoted: msg.data});
+    var user, count;
+      if (query[1].includes('/')) {
+         var split = query[1].split('/');
+         count = split[1];
+         user = split[0];
+    var value = parseInt(count);
+    var res = await insta.getStory(user)
+    var buffer = await get.skbuffer(res.result.data[value].url)
+    await msg.client.sendMessage(msg.jid, buffer, MessageType.image, { mimetype: Mimetype.jpg, caption: 'Success!', quoted: msg.data});
     }));
