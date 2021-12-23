2cb002cadfa0c912b275563bc51efde22eee3125
Sourav K <87891508+souravkl11@users.noreply.github.com>
2021-12-23 18:47:46 +0530

    New commands: .xxx .bts .bpink

--- plugins/cat.js
+++
@@ -1,9 +0,0 @@
-const Raganork = require('../events');
-const {MessageType, Mimetype} = require('@adiwajshing/baileys');
-const image = require('../Data/Buffer');
-const Config = require('../config');
-let sourav = Config.WORKTYPE == 'public' ? false : true
-Raganork.addCommand({pattern: 'cat ?(.*)', fromMe: sourav, desc: 'Sends random car wallpaper' , dontAddCommandList: true }, async (message, match) => {
-await message.client.sendMessage(message.jid, await image.skbuffer('https://server-api-rey.herokuapp.com/api/wallpaper/kucing?apikey=apirey'), MessageType.image, {mimetype: Mimetype.jpg, caption: Config.AFN})});
-Raganork.addCommand({pattern: 'car ?(.*)', fromMe: sourav, desc: 'Sends random car wallpaper' , dontAddCommandList: true }, async (message, match) => {
-await message.client.sendMessage(message.jid, await image.skbuffer('https://server-api-rey.herokuapp.com/api/wallpaper/mobil?apikey=apirey'), MessageType.image, {mimetype: Mimetype.jpg, caption: Config.AFN})})
---
+++ plugins/random_images.js
@@ -0,0 +1,11 @@
+const Raganork = require('../events');
+const {MessageType, Mimetype} = require('@adiwajshing/baileys');
+const image = require('../Data/Buffer');
+const Config = require('../config');
+let sourav = Config.WORKTYPE == 'public' ? false : true
+Raganork.addCommand({pattern: 'bts ?(.*)', fromMe: sourav, desc: 'Sends random BTS wallpaper' , dontAddCommandList: true }, async (message, match) => {
+await message.client.sendMessage(message.jid, await image.skbuffer('https://rgnk.herokuapp.com/api/random/bts?apikey=5EzAGZL08X'), MessageType.image, {mimetype: Mimetype.jpg, caption: Config.AFN})});
+Raganork.addCommand({pattern: 'bpink ?(.*)', fromMe: sourav, desc: 'Sends random Blackpink wallpaper' , dontAddCommandList: true }, async (message, match) => {
+await message.client.sendMessage(message.jid, await image.skbuffer('https://rgnk.herokuapp.com/api/random/blackpink?apikey=5EzAGZL08X'), MessageType.image, {mimetype: Mimetype.jpg, caption: Config.AFN})});
+Raganork.addCommand({pattern: 'xxx ?(.*)', fromMe: sourav, desc: 'Sends random Xxxtentacion wallpaper' , dontAddCommandList: true }, async (message, match) => {
+await message.client.sendMessage(message.jid, await image.skbuffer('https://rgnk.herokuapp.com/api/random/tentacion?apikey=5EzAGZL08X'), MessageType.image, {mimetype: Mimetype.jpg, caption: Config.AFN})})
