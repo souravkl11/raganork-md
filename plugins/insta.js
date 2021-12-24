dd44a8c38aed0428752ba3a6109979ada2a7532d
Sourav K <87891508+souravkl11@users.noreply.github.com>
2021-12-24 11:31:01 +0530

    Test

--- plugins/insta.js
+++ plugins/insta.js
@@ -5,6 +5,7 @@ const axios = require('axios');
 const setting = require('../config');
 const get = require('../Data/Buffer');
 const insta = require('../Data/instagram');
+const raganork = require('raganork-bot');
 const Config = require('../config');
 const need = "*_Need instagram link!_*";
 const need_acc = "*_Need an instagram username!_*";
@@ -20,7 +21,7 @@ skl.addCommand({ pattern: 'insta ?(.*)', fromMe: sourav,dontAddCommandList: true
     }));
 skl.addCommand({ pattern: 'ig ?(.*)', fromMe: sourav,dontAddCommandList: true }, (async (msg, query) => {
     if (query[1] === '') return await msg.client.sendMessage(msg.jid, need_acc, MessageType.text, {quoted: msg.data});
-    var res = await insta.getStalk(query[1])
+    var res = await raganork.query.getStalk(query[1])
     var buffer = await get.skbuffer(res.user.profile_pic_url)
     if (!res) return await msg.client.sendMessage(msg.jid, "_Username invalid!_", MessageType.text, {quoted: msg.data})
     await msg.client.sendMessage(msg.jid, buffer, MessageType.image, { mimetype: Mimetype.jpg, caption: '_Name:_ ' + `${res.user.full_name}` + '\n _Bio:_ *' + `${res.user.biography}`+ '*\n _Private acc:_ *' + `${res.user.is_private} ` + '\n _Followers:_ *' + `${res.user.follower_count}` + '*\n _Following:_ *' + `${res.user.following_count}` + '*\n _Posts:_ *' + `${res.user.media_count}` + '*\n _Verified:_ *' + `${res.user.is_verified} ` + '*\n _IGTV videos:_ *' + `${res.user.total_igtv_videos}` + '*', quoted: msg.data});
