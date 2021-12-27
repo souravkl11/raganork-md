37b30b3defe3a1233e00bc9fbec81ca2fdb093c2
Sourav K <87891508+souravkl11@users.noreply.github.com>
2021-12-27 18:26:32 +0530

    Fix

--- plugins/music_finder.js
+++ plugins/music_finder.js
@@ -1,84 +1,86 @@
 // Music finder - souravkl11/raganork
-function B(h, j) {
-    return v(h - 0xbb, j);
-}(function (h, j) {
+(function (h, j) {
     function p(h, j) {
-        return v(j - 0x22b, h);
+        return v(h - '0x37b', j);
     }
     var K = h();
     while (!![]) {
         try {
-            var U = parseInt(p(0x2fd, 0x311)) / (-0x883 + 0x862 * -0x3 + 0x21aa) + parseInt(p(0x307, 0x310)) / (0x538 * -0x1 + -0xfb6 + 0x14f0) + parseInt(p(0x320, '0x326')) / (0x2f * 0x5e + 0x1 * -0x60f + -0xb30) * (-parseInt(p('0x309', '0x325')) / (0x11de + -0x1291 * -0x1 + -0x246b)) + parseInt(p(0x30f, '0x31a')) / (-0x2618 + -0x8ec + -0x2f09 * -0x1) + -parseInt(p('0x330', '0x32a')) / (-0x84b * -0x3 + 0x1ce0 + -0x35bb) * (-parseInt(p(0x2fe, 0x300)) / (-0x6 * -0x66 + -0x5b * 0x7 + 0x20)) + -parseInt(p('0x32f', 0x334)) / (0xaa0 + 0x1e4e + -0x15d * 0x1e) * (-parseInt(p('0x32f', 0x315)) / (-0x1c3e + 0x2c * -0x8e + -0x34af * -0x1)) + parseInt(p(0x33e, 0x327)) / (0x24bd + 0x3 * 0x20d + -0x2ada) * (-parseInt(p(0x315, '0x30f')) / (0xbc5 * -0x3 + 0x8 * 0x185 + 0x1732));
+            var U = parseInt(p(0x419, 0x431)) / (-0x9e2 + 0x1 * 0x11d2 + 0x3 * -0x2a5) * (-parseInt(p('0x42c', '0x433')) / (-0x30b * -0x1 + 0x4d8 + 0x1 * -0x7e1)) + parseInt(p(0x439, 0x430)) / (0xa88 + 0x1a7e * 0x1 + -0x767 * 0x5) * (-parseInt(p('0x443', 0x448)) / (-0x1811 + 0x594 * 0x1 + 0x1281)) + -parseInt(p(0x40b, '0x421')) / (0x2662 + -0xa5 + 0x25b8 * -0x1) * (parseInt(p('0x420', '0x421')) / (0x112 * 0x3 + 0x155 * 0x12 + -0x90e * 0x3)) + -parseInt(p('0x41a', '0x41c')) / (0x6 * -0x182 + 0x719 * 0x5 + 0x17 * -0x126) * (parseInt(p('0x422', '0x43b')) / (-0x159d + -0x19d3 + 0x2f78)) + parseInt(p(0x421, 0x439)) / (0x261a + -0x23d + -0x23d4) * (parseInt(p('0x436', 0x43b)) / (0x1 * 0x1845 + -0xd44 * 0x1 + 0x1 * -0xaf7)) + parseInt(p(0x41f, '0x42c')) / (0x1998 + 0x19 * -0xb3 + 0x409 * -0x2) + parseInt(p(0x442, 0x438)) / (-0x177d * 0x1 + 0x2363 + 0x2 * -0x5ed);
             if (U === j) break;
             else K['push'](K['shift']());
         } catch (c) {
             K['push'](K['shift']());
         }
     }
-}(F, 0x6b8e3 + 0xd022 * 0x10 + 0x16 * -0x789e));
+}(F, 0x557d4 + 0x7ff9c + 0x125 * 0xf4));
 
 function v(u, Z) {
     var h = F();
     return v = function (j, K) {
-        j = j - (0x17f5 * -0x1 + 0x1831 + 0x93);
+        j = j - (-0xed * 0x1d + -0x1cce + 0x3834);
         var U = h[j];
         return U;
     }, v(u, Z);
 }
-var New = require(B('0x19c', 0x19a)),
+var New = require(B('0x3b2', '0x3b4')),
     {
         MessageType,
         Mimetype
-    } = require(B('0x1ac', '0x18e') + 'ng/baileys');
-const setting = require(B(0x18b, 0x194));
-var find = require(B(0x1a3, '0x1b6') + 'ot');
-let sourav = setting[B(0x19b, 0x1aa)] == B('0x191', 0x194) ? ![] : !![];
+    } = require(B(0x3b5, '0x3cb') + B('0x390', '0x39e'));
+const setting = require(B('0x3a8', '0x39d'));
+
+function B(h, j) {
+    return v(h - 0x2f8, j);
+}
+var find = require(B(0x3a0, 0x3a5) + 'ot');
+let sourav = setting[B('0x3a7', '0x3ad')] == 'public' ? ![] : !![];
 var u = {};
-u[B('0x196', 0x1b4)] = B('0x1c2', '0x1cf'), u[B('0x199', '0x1b5')] = !![], New[B('0x1c0', '0x1aa')](u, async (h, j) => {
+u['pattern'] = 'find ?(.*)', u[B(0x3ac, '0x3be')] = !![], New[B('0x3ba', 0x3b5)](u, async (h, j) => {
     function z(h, j) {
-        return B(h - -'0x7a', j);
+        return B(j - -0x448, h);
     }
-    if (!h[z(0x13a, '0x13d') + z(0x13e, 0x15c)][z('0x134', 0x115)] && !h['reply_mess' + z('0x13e', 0x12d)][z(0x11d, '0x11a')] && !h[z('0x13a', '0x123') + z('0x13e', '0x12a')][z(0x12f, 0x13b)] && !h[z('0x13a', '0x157') + z(0x13e, '0x150')][z(0x128, '0x134')]) {
-        var K = await h[z('0x115', '0x105')][z(0x135, '0x14e') + z('0x120', 0x108) + z('0x124', '0x135')]({
+    if (!h[z(-'0xb3', -0x9a) + z(-0x8a, -'0x90')]['text'] && !h['reply_mess' + z(-0x9a, -0x90)]['video'] && !h[z(-'0x88', -'0x9a') + z(-0x82, -0x90)]['sticker'] && !h[z(-'0xb1', -'0x9a') + z(-'0x75', -0x90)]['image']) {
+        var K = await h[z(-0xc8, -'0xad')][z(-'0x79', -0x8c) + z(-0x97, -'0x9d') + 'Message']({
                 'key': {
-                    'remoteJid': h['reply_mess' + 'age'][z('0x147', 0x148)],
-                    'id': h['reply_mess' + z(0x13e, '0x120')]['id']
+                    'remoteJid': h['reply_mess' + z(-'0xad', -0x90)][z(-'0xa7', -'0x99')],
+                    'id': h[z(-0x93, -'0x9a') + 'age']['id']
                 },
-                'message': h[z(0x13a, '0x11d') + z('0x13e', 0x158)][z(0x119, '0x124')][z('0x113', 0x110) + z('0x13e', 0x15c)]
+                'message': h[z(-0x7c, -0x9a) + z(-'0xa1', -0x90)][z(-'0x7c', -'0x97')]['quotedMess' + z(-0xa7, -0x90)]
             }),
-            U = setting[z('0x12d', '0x129')],
-            c = await find[z('0x133', 0x120)][z(0x14c, '0x12e')](K, U);
-        console['log'](c);
-        if (c[z(0x138, 0x140)]) {
-            let w = '_Title:_ *' + c['result'][z('0x145', 0x14a)] + (z('0x139', 0x150) + z(0x114, 0xfc)) + c[z(0x138, '0x131')]['album'] + (z(0x131, 0x12e) + ':_ *') + c['result']['artist'] + (z(0x137, '0x155') + z(0x114, '0xfc')) + c['result'][z('0x14b', 0x136)] + ('* \x0a_Releas' + z(0x12e, 0x137)) + c[z(0x138, 0x152)]['release_da' + 'te'] + (z(0x11a, '0x10d') + 'ink:_ ') + ('https://ww' + z('0x118', '0x116') + z(0x136, '0x130') + z('0x12c', '0x149') + z('0x112', 0x121)) + c[z('0x138', '0x11c')]['title'][z(0x11e, 0x10f)](' ')['join']('') + '*';
-            return await h[z('0x115', 0x10f)]['sendMessag' + 'e'](h['jid'], w, MessageType[z('0x134', 0x14d)], {
-                'quoted': h[z(0x119, '0x126')]
+            U = setting[z(-0xc6, -'0xb5')],
+            c = await find[z(-0xbd, -0xb9)][z(-0x86, -'0xa2')](K, U);
+        console[z(-0xa3, -0xaf)](c);
+        if (c['result']) {
+            let w = z(-'0x93', -0x8d) + c[z(-'0xb6', -'0xbc')][z(-0xb5, -'0xc3')] + (z(-0xac, -'0xa6') + '_ *') + c[z(-0xad, -'0xbc')][z(-'0xaa', -0xb7)] + (z(-0xa4, -'0x8f') + z(-'0xa3', -0x8b)) + c[z(-'0xb0', -'0xbc')][z(-'0xa7', -0xb4)] + (z(-'0x7c', -'0x91') + z(-'0x98', -0x87)) + c[z(-'0xc1', -'0xbc')][z(-0x91, -'0xa7')] + (z(-0xbb, -0xbe) + z(-0xa9, -0xb3)) + c['result'][z(-'0xac', -'0xba') + 'te'] + ('* \x0a_Song l' + 'ink:_ ') + (z(-'0xa3', -'0x8a') + 'w.youtube.' + z(-'0xaa', -'0xa3') + z(-'0x99', -'0x94') + 'uery=') + c[z(-'0xa5', -0xbc)][z(-0xcf, -'0xc3')]['split'](' ')['join']('') + '*';
+            return await h[z(-0xbf, -0xad)][z(-0xcd, -'0xc2') + 'e'](h[z(-0x90, -0x99)], w, MessageType[z(-'0xa3', -'0xbf')], {
+                'quoted': h['data']
             });
         }
-        if (c[z('0x123', '0x140')]) return await h[z(0x115, '0x126')][z(0x142, 0x15f) + 'e'](h[z(0x147, '0x133')], z(0x143, 0x141) + c[z('0x123', '0x10c')][z('0x11b', '0x126')] + '_', MessageType[z('0x134', 0x14f)], {
-            'quoted': h[z(0x119, 0x104)]
+        if (c[z(-'0xb5', -'0xc1')]) return await h[z(-0x91, -0xad)]['sendMessag' + 'e'](h['jid'], z(-0xcf, -'0xbd') + c['error'][z(-0xa6, -0x9b)] + '_', MessageType['text'], {
+            'quoted': h['data']
         });
-        if (!c[z(0x138, 0x153)]) return await h[z('0x115', '0xf8')][z('0x142', 0x154) + 'e'](h[z(0x147, 0x149)], z(0x144, 0x126), MessageType[z(0x134, 0x14c)], {
-            'quoted': h[z(0x119, '0x11d')]
+        if (!c[z(-'0xc2', -0xbc)]) return await h[z(-'0xbf', -'0xad')][z(-0xca, -0xc2) + 'e'](h[z(-'0x84', -0x99)], z(-0x9e, -'0xb0'), MessageType['text'], {
+            'quoted': h[z(-0x91, -'0x97')]
         });
-    } else return await h[z('0x115', 0x10b)][z(0x142, '0x14b') + 'e'](h[z(0x147, '0x137')], z('0x13f', 0x145) + z(0x110, '0x103') + '_', MessageType[z(0x134, 0x14b)], {
-        'quoted': h[z(0x119, 0xfc)]
+    } else return await h[z(-0x8e, -0xad)][z(-0xab, -'0xc2') + 'e'](h[z(-'0xac', -'0x99')], z(-0xb3, -'0xbb') + z(-0x90, -0x9e) + '_', MessageType['text'], {
+        'quoted': h['data']
     });
 });
-if (setting[B('0x19b', 0x19a)] === B(0x191, 0x1a2)) {
+if (setting[B('0x3a7', 0x3aa)] === B('0x3a4', 0x3bf)) {
     var Z = {};
-    Z[B(0x196, 0x187)] = B(0x1c2, '0x1ab'), Z[B('0x199', '0x194')] = ![], New[B('0x1c0', '0x1a3')](Z, async (h, j) => {
+    Z[B(0x3b0, 0x39b)] = 'find ?(.*)', Z[B(0x3ac, '0x3c6')] = ![], New['addCommand'](Z, async (h, j) => {
         function y(h, j) {
-            return B(h - -'0x24', j);
+            return B(j - -0x2a7, h);
         }
-        return await h[y('0x16b', '0x176')][y('0x198', '0x187') + 'e'](h[y('0x19d', '0x1a4')], y(0x19f, '0x1b2') + y('0x180', 0x185) + 'use this c' + y(0x197, '0x1a9'), MessageType[y('0x18a', '0x189')], {
-            'quoted': h[y('0x16f', '0x158')]
+        return await h[y('0xf3', '0xf4')][y('0xdd', 0xdf) + 'e'](h[y('0x120', 0x108)], '_Only the ' + y('0xd7', 0xf3) + y('0xff', '0xfc') + y('0xe6', 0xeb), MessageType['text'], {
+            'quoted': h['data']
         });
     });
 }
 
 function F() {
-    var m = ['8348351oMWAcB', '11362wsTIcp', '543832ZHqYWy', 'image', 'raganork-b', 'owner can ', '4357629nQNXBC', 's?search_q', 'ZEKAIS_KEY', 'e date:_ *', 'sticker', '3524065baWYEt', '* \x0a_Artist', '@adiwajshi', 'query', 'text', 'downloadAn', 'com/result', '* \x0a_Label:', 'result', '* \x0a_Album:', 'reply_mess', '4748LBGBJb', '2790AVEeJL', '10jkMuXo', 'age', '_Reply to ', '4428522xAZRcc', 'ommand!_', 'sendMessag', '_Error ', '_Failed!_', 'title', 'addCommand', 'jid', 'find ?(.*)', '_Only the ', '8AhbCOx', 'label', 'music', 'any music!', '../config', 'uery=', 'quotedMess', '_ *', 'client', '7MPVoLu', 'public', 'w.youtube.', 'data', '* \x0a_Song l', 'error_code', 'pattern', 'video', 'split', 'fromMe', 'dSaveMedia', 'WORKTYPE', '../events', 'error', 'Message'];
+    var m = ['ng/baileys', 'album', 'ommand!_', 'ZEKAIS_API', 'artist', 'e date:_ *', '1FIOyzc', '358813JpnHDE', '_Failed!_', 'log', 'owner can ', 'client', '17335714SshOvQ', '1542ZVKqrg', '27trgrrh', '272aJyxFk', 'raganork-b', 'label', '* \x0a_Album:', 'use this c', 'public', 'com/result', 'music', 'WORKTYPE', '../config', '1158934uRYHTc', 'any music!', 'dSaveMedia', 'fromMe', 'error_code', 'reply_mess', 'jid', 'pattern', 'data', '../events', '4845990xmsuKx', 's?search_q', '@adiwajshi', '3UwoBcE', '* \x0a_Label:', 'age', '* \x0a_Artist', 'addCommand', '_Title:_ *', 'downloadAn', ':_ *', 'https://ww', '38454564sBLxkW', '5564192tIEsJU', '_ *', 'title', 'sendMessag', 'error', '30645zYQLPS', 'text', '* \x0a_Releas', '_Error ', 'result', '_Reply to ', 'release_da', 'query'];
     F = function () {
         return m;
     };
