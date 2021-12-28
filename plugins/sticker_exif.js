382fdc44885a8e86aa74b7e5ffd2bc5bf6bfe00c
Sourav K <87891508+souravkl11@users.noreply.github.com>
2021-12-28 20:21:29 +0530

    fix

--- plugins/sticker_exif.js
+++ plugins/sticker_exif.js
@@ -7,7 +7,7 @@ let v = w.SUPPORT3
 let i = require('raganork-bot');
 let a = w.WORKTYPE == 'public' ? false : true;
 let ffmpeg = require('fluent-ffmpeg');
-e.addCommand({pattern: 'steal ?(.*)', fromMe: a, desc:'Changes sticker pack & author name!'}, (async (m, match) => { 
+e.addCommand({pattern: 'take ?(.*)', fromMe: a, desc:'Changes sticker pack & author name!'}, (async (m, match) => { 
 var q = await m.client.downloadAndSaveMediaMessage({key: {remoteJid: m.reply_message.jid,id: m.reply_message.id},message: m.reply_message.data.quotedMessage});
 var s = w.SOURAVKL11.split('|');
 var au = s[1];
@@ -18,7 +18,7 @@ ffmpeg(q)
 .on('end', async () => {
 var res = await i.query.sticker('ex.png',au,p,v)
 await m.client.sendMessage(m.jid, await i.query.skbuffer(res),MessageType.sticker,{quoted:m.data});})}));
-e.addCommand({pattern: 'st ?(.*)', fromMe: a, desc:'Sets sticker pack & author name with given ones.'}, (async (m, t) => { 
+e.addCommand({pattern: 'wm ?(.*)', fromMe: a, desc:'Sets sticker pack & author name with given ones.'}, (async (m, t) => { 
 var q = await m.client.downloadAndSaveMediaMessage({key: {remoteJid: m.reply_message.jid,id: m.reply_message.id},message: m.reply_message.data.quotedMessage});
 var au,p;
 if (t[1].includes('|')) {
