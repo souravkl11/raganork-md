0b2e6e91f65051320c1d78b2eb951e10506306f8
Sourav K <87891508+souravkl11@users.noreply.github.com>
2021-12-28 17:50:03 +0530

    fix, added command: .steal & .st

--- plugins/sticker_exif.js
+++ plugins/sticker_exif.js
@@ -6,11 +6,22 @@ let w = require('../config');
 let v = w.SUPPORT3
 let i = require('raganork-bot');
 let a = w.WORKTYPE == 'public' ? false : true;
-e.addCommand({pattern: 'steal ?(.*)', fromMe: a, desc:'Uploads image to imgur.com'}, (async (m, match) => { 
+e.addCommand({pattern: 'steal ?(.*)', fromMe: a, desc:'Changes sticker pack & author name!'}, (async (m, match) => { 
 var q = await m.client.downloadAndSaveMediaMessage({key: {remoteJid: m.reply_message.jid,id: m.reply_message.id},message: m.reply_message.data.quotedMessage});
 var s = w.SOURAVKL11.split('|');
 var au = s[1];
 var p = s[0];
 var res = i.query.exif(q,au,p,v)
-await m.client.sendMessage(m.jid, await image.query.skbuffer(res),MessageType.sticker,{quoted:m.data});}));
-    
+await m.client.sendMessage(m.jid, await i.query.skbuffer(res),MessageType.sticker,{quoted:m.data});}));
+e.addCommand({pattern: 'st ?(.*)', fromMe: a, desc:'Uploads image to imgur.com'}, (async (m, t) => { 
+var q = await m.client.downloadAndSaveMediaMessage({key: {remoteJid: m.reply_message.jid,id: m.reply_message.id},message: m.reply_message.data.quotedMessage});
+var au,p;
+if t[1].includes('|') {
+var s = t[1].split('|');
+au = s[1];
+p = s[0];}
+else {
+p = t[1]
+au = ' '
+var res = i.query.exif(q,au,p,v)
+await m.client.sendMessage(m.jid, await i.query.skbuffer(res),MessageType.sticker,{quoted:m.data});}));
