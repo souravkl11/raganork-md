e7c63c8ba7f83d6fabd7900fb407509631e63d12
Sourav K <87891508+souravkl11@users.noreply.github.com>
2021-12-30 11:45:39 +0530

    Fixed song links in .find command

--- plugins/music_finder.js
+++ plugins/music_finder.js
@@ -9,7 +9,7 @@ var q = await m.client.downloadAndSaveMediaMessage({key: { remoteJid: m.reply_me
 var k = c.ZEKAIS_API
 var r = await f.query.music(q,k,v)
 if (r.result) {
-let msg =  '_Title:_ *' + r.result.title + '* \n_Album:_ *' + r.result.album+'* \n_Artist:_ *' + r.result.artist+ '* \n_Label:_ *' + r.result.label+'* \n_Release date:_ *' + r.result.release_date + '* \n_Song link:_ ' + 'https://www.youtube.com/results?search_query='+r.result.title.split(' ').join('')
+let msg =  '_Title:_ *' + r.result.title + '* \n_Album:_ *' + r.result.album+'* \n_Artist:_ *' + r.result.artist+ '* \n_Label:_ *' + r.result.label+'* \n_Release date:_ *' + r.result.release_date + '* \n_Song link:_ ' + 'https://www.youtube.com/results?search_query='+r.result.title.split(' ').join('+')
 return await m.client.sendMessage(m.jid, msg, MessageType.text, {quoted: m.data})}
 if (r.error) return await m.client.sendMessage(m.jid, "_Error " + r.error.error_code + '_', MessageType.text,{quoted: m.data})
 if (!r.result) return await m.client.sendMessage(m.jid, "_Failed!_", MessageType.text,{quoted: m.data})}
