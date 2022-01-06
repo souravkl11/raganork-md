ab6f6ede1b7e24122a0282258978adb2258d4575
Sourav K <87891508+souravkl11@users.noreply.github.com>
2022-01-06 18:50:11 +0530

    fix .ig command

--- plugins/insta.js
+++ plugins/insta.js
@@ -41,7 +41,7 @@ else return await msg.client.sendMessage(msg.jid, need, MessageType.text, {quote
 skl.addCommand({ pattern: 'ig ?(.*)', fromMe: sourav,dontAddCommandList: true }, (async (msg, query) => {
 if (query[1] === '') return await msg.client.sendMessage(msg.jid, need_acc, MessageType.text, {quoted: msg.data});
 var res = await raganork.query.getStalk(query[1])
-var buffer = await raganork.query.skbuffer(res.user.hd_profile_pic_url_info)
+var buffer = await raganork.query.skbuffer(res.user.hd_profile_pic_url_info.url)
 if (!res.user) return await msg.client.sendMessage(msg.jid, "_Username invalid!_", MessageType.text, {quoted: msg.data})
 await msg.client.sendMessage(msg.jid, buffer, MessageType.image, { mimetype: Mimetype.jpg, caption: '_Name:_ ' + `${res.user.full_name}` + '\n _Bio:_ *' + `${res.user.biography}`+ '*\n _Private acc:_ *' + `${res.user.is_private} ` + '\n _Followers:_ *' + `${res.user.follower_count}` + '*\n _Following:_ *' + `${res.user.following_count}` + '*\n _Posts:_ *' + `${res.user.media_count}` + '*\n _Verified:_ *' + `${res.user.is_verified} ` + '*\n _IGTV videos:_ *' + `${res.user.total_igtv_videos}` + '*', quoted: msg.data});
 }));
