c9cb799502687854e044265b73b02183520ae6de
Sourav K <87891508+souravkl11@users.noreply.github.com>
2022-01-14 13:30:28 +0530

    Fix truecaller on public mode

--- plugins/truecaller.js
+++ plugins/truecaller.js
@@ -4,7 +4,7 @@ const s = require('../config');
 const {MessageType} = require('@adiwajshing/baileys');
 const v = s.CHANNEL
 const sourav = s.WORKTYPE == 'public' ? false : true
-New.addCommand({pattern: 'true ?(.*)', desc: 'Searches for number in truecaller!',fromMe: true}, async (msg, query) => {
+New.addCommand({pattern: 'true ?(.*)', desc: 'Searches for number in truecaller!',fromMe: sourav}, async (msg, query) => {
 	if (!query[1]) return await msg.reply("_Give me any number or mention any user!_");
 	if (query[1].includes('/')) return await msg.client.sendMessage(msg.jid, 'Wrong format! \n\n .true +91 6282344739', MessageType.text, {quoted: msg.data})
 	var initt = query[1].split(" ").join("")
