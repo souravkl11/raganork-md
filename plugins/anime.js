969b1b7faadb4520ed1c28a2daa0031621ccc915
Sourav K <87891508+souravkl11@users.noreply.github.com>
2022-01-17 12:53:58 +0530

    v4 stable

--- plugins/anime.js
+++ plugins/anime.js
@@ -96,7 +96,7 @@ if (Config.WORKTYPE == 'private') {
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made by PLK'})
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: Config.AFN})
 
     }));
 }
