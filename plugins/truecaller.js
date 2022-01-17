f61a83f9941d6618960679d30af24ce7fe0d2e4f
Sourav K <87891508+souravkl11@users.noreply.github.com>
2022-01-17 10:54:20 +0530

    .true now supports mention and reply message

--- plugins/truecaller.js
+++ plugins/truecaller.js
@@ -5,9 +5,19 @@ const {MessageType} = require('@adiwajshing/baileys');
 const v = s.CHANNEL
 const sourav = s.WORKTYPE == 'public' ? false : true
 New.addCommand({pattern: 'true ?(.*)', desc: 'Searches for number in truecaller!',fromMe: sourav}, async (msg, query) => {
-	if (!query[1]) return await msg.reply("_Give me any number or mention any user!_");
+	if (!query[1] && !msg.reply_message) return await msg.reply("_Give me any number or reply to any user!_");
 	if (query[1].includes('/')) return await msg.client.sendMessage(msg.jid, 'Wrong format! \n\n .true +91 6282344739', MessageType.text, {quoted: msg.data})
-	var initt = query[1].split(" ").join("")
+	var go;
+	if (msg.reply_message) go = msg.reply_message.jid.split('@')[0]
+	else if (query[1]) go = query[1]
+	else if (msg.mention) {
+	var mm = '';
+            msg.mention.map(async (user) => {
+                mm += user.split('@')[0];
+            });
+	go = mm
+	} 
+	var initt = go.split(" ").join("")
     var number = initt.replace('+','')
     const res = await truecaller.query.find(number,'',v)
 		await msg.client.sendMessage(msg.jid, '*RECIEVED DETAILS FROM TRUECALLER!* \n\n' + '*✅' + "Number:" +'* ```' + res.phones[0].e164Format + '```\n' +
