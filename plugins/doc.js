b7300080e896e31a95ed870406b6d336029700d8
Sourav K <87891508+souravkl11@users.noreply.github.com>
2022-01-21 12:41:39 +0530

    Fixed ,doc

--- plugins/doc.js
+++ plugins/doc.js
@@ -7,8 +7,8 @@ let i = require('raganork-bot');
 const { fromBuffer } = require('file-type')
 let fm = c.WORKTYPE == 'public' ? false : true
 e.addCommand({ pattern: 'doc', fromMe: fm, desc: 'Converts media messages to documents with given filenames' }, async (m, q) => {
-let fn = await m.client.downloadAndSaveMediaMessage({key: {remoteJid: m.reply_message.jid,id: m.reply_message.id},message: m.reply_message.data.quotedMessage})
+let fn = await m.client.downloadMediaMessage({key: {remoteJid: m.reply_message.jid,id: m.reply_message.id},message: m.reply_message.data.quotedMessage})
 let fnm = q[1] ? q[1] : 'File'
 let mime = await fromBuffer(fn)
-await m.client.sendMessage(m.jid,f.readFileSync(fn), MessageType.document, {filename: fm+p.extname(fn), mimetype: mime.mime});
+await m.client.sendMessage(m.jid,fn, MessageType.document, {filename: fm+p.extname(fn), mimetype: mime.mime});
 })
