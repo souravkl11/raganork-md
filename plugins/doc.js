675cb16f2712b0211ec3730039f81944e07f3013
Sourav K <87891508+souravkl11@users.noreply.github.com>
2022-01-21 12:22:13 +0530

    Fix .doc command

--- plugins/doc.js
+++ plugins/doc.js
@@ -4,9 +4,11 @@ let { MessageType } = require('@adiwajshing/baileys')
 let e = require('../events');
 let c = require('../config');
 let i = require('raganork-bot');
+const { fromBuffer } = require('file-type')
 let fm = c.WORKTYPE == 'public' ? false : true
 e.addCommand({ pattern: 'doc', fromMe: fm, desc: 'Converts media messages to documents with given filenames' }, async (m, q) => {
 let fn = await m.client.downloadAndSaveMediaMessage({key: {remoteJid: m.reply_message.jid,id: m.reply_message.id},message: m.reply_message.data.quotedMessage})
 let fnm = q[1] ? q[1] : 'File'
-await m.client.sendMessage(m.jid,f.readFileSync(fn), MessageType.document, {filename: fm+p.extname(fn)});
+let mime = await fromBuffer(fn)
+await m.client.sendMessage(m.jid,f.readFileSync(fn), MessageType.document, {filename: fm+p.extname(fn), mimetype: mime.mime});
 })
