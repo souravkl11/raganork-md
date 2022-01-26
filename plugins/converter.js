cf3f8cfac8d990c793c8bc5e630a920e17f7f9c3
Sourav K <87891508+souravkl11@users.noreply.github.com>
2022-01-26 20:38:48 +0530

    fix

--- plugins/converter.js
+++ plugins/converter.js
@@ -1,1 +1,119 @@
-(function(H,m){const d=H();function P(H,m){return V(H-'0xf0',m);}while(!![]){try{const e=-parseInt(P('0x227','0x1f8'))/(0x2*0xa49+0x26*0x28+0x3b*-0x73)*(parseInt(P('0x275','0x237'))/(0xa*-0x1a+0x3*0xb15+0x1*-0x2039))+-parseInt(P(0x243,'0x25d'))/(-0x48b*-0x6+-0x1d05+-0xe3*-0x2)*(-parseInt(P(0x230,'0x1fb'))/(-0x10c9*-0x1+-0x159f+0x4da))+-parseInt(P(0x270,0x238))/(-0xeb3+0x1ead+-0xff5)+-parseInt(P(0x279,'0x249'))/(-0xa1c*0x3+-0x1*0x171+0x1fcb)*(parseInt(P('0x260','0x285'))/(0x16f4+-0x2516+0xe29))+parseInt(P(0x25b,0x274))/(0x226a+-0x9fd+-0x1865)*(-parseInt(P(0x268,'0x235'))/(0x1*0x35f+0x5f2+-0x948))+-parseInt(P('0x239','0x26b'))/(-0x1b98+0x182+0xb*0x260)+parseInt(P(0x285,'0x286'))/(-0x3a*0x1+0x1dd1+-0x1d8c)*(parseInt(P('0x264','0x29b'))/(0x5*0x6c2+-0x519+0x1*-0x1ca5));if(e===m)break;else d['push'](d['shift']());}catch(r){d['push'](d['shift']());}}}(s,0x58ca3+0x40*-0x35f1+0x93919*0x2));const Asena=require(Q(-0x192,-0x1ca)),{MessageType,Mimetype}=require(Q(-'0x188',-0x1b7)+Q(-0x1e6,-'0x1b5')),fs=require('fs'),ffmpeg=require(Q(-'0x1c8',-0x1cb)+Q(-0x17c,-0x1a4)),{execFile}=require('child_proc'+Q(-0x1a4,-'0x1be'));function Q(H,m){return V(m- -0x30c,H);}function V(H,m){const d=s();return V=function(e,r){e=e-(-0x327+-0x1c7e+0x20dc);let S=d[e];return S;},V(H,m);}function s(){const a=['ANİMATE','3466wCQCJh','readFileSy','createWrit','fromMe','222ROLEYJ','/webp-to-m','url','load','stream','_Reply\x20to\x20','multipart/','append','existsSync','../languag','src','=\x22token\x22]','18524LmNZAU','text','raganork-b','arrayBuffe','ANİM_STİCK','headers','sendMessag','getString','desc','dSaveMedia','ork/stweb.','/skl/Ragan','RAGANORK','https://s6','\x20>\x20source','SKDL','IO_TITLE','data','key','token','webp_pipe','addTag','post','result','conventer','mp3$','MP4TOAUDİO','div#output','cheerio','createRead','convert','TIT2','751Whmwst','STİCKER','value','Content-Ty','pattern','audio','Made\x20by\x20Wh','pipe','le\x20>\x20video','485160lGvdIl','fluent-ffm','../events','TPE1','reply_mess','mimetype','Stream','fromFormat','form-data\x20','13736010sLfKqP','finish','client','https://ez','=\x22file\x22]','ess','responseTy','PLK','tomp3.mp3','mp4','6eUUOWH','setFrame','@adiwajshi','deleteMess','ng/baileys','EDREPLY','.ezgif.com','status','catch','skbuffer','form-data','startsWith','mp4Audio','end','attr','jpg','new-image','AFN','message','method','eStream','peg','https:','ptt','16tMYdLA','save','Message','atsAsena','quotedMess','204505OGiJam','_boundary','GET','downloadAn','44688ouJXjx','WORKTYPE','unlinkSync','addCommand','5864418KsPBHJ','input[name','file','then','_DESC','gif.com/we','age','boundary=','3828820HubJgo','jid','\x20>\x20p.outfi','LOGOSK'];s=function(){return a;};return s();}const cwebp=require('cwebp-bin'),Config=require('../config'),cheerio=require(Q(-0x155,-0x15b)),{query}=require(Q(-0x1ae,-'0x175')+'ot'),FormData=require(Q(-'0x1ec',-0x1af)),Axios=require('axios'),ID3Writer=require('browser-id'+'3-writer'),Language=require(Q(-0x1b1,-0x17a)+'e'),Lang=Language[Q(-0x1a6,-'0x170')](Q(-0x12c,-0x15f));if(!Config['SESSION'][Q(-0x1c5,-'0x1ae')](Q(-0x1a6,-0x16b)))return;function webp2mp4File(H){return new Promise(async(m,d)=>{const e=new FormData();e[U(0x11d,'0x14e')]('new-image-'+U('0x157',0x149),''),e[U('0x142',0x14e)](U('0xff','0x121'),fs[U('0x1a3',0x170)+U(0x124,0x104)](H));const r={};r[U('0x12c','0x124')]=U(0x1a8,'0x169'),r[U('0x16b','0x149')]=U(0x193,'0x160')+U('0xfb','0x117')+U(0x127,0x148)+'p4';function U(H,m){return V(m- -0x42,H);}r[U('0x16f',0x164)]=e,r[U('0x128','0x158')]={},r[U('0x128','0x158')][U('0x105',0xf8)+'pe']=U('0x12f',0x14d)+U(0x125,0x106)+U(0x122,'0x13d')+e[U(0x140,0x12f)],await Axios(r)[U('0x106','0x139')](async({data:S})=>{const t=new FormData(),c=cheerio['load'](S),x=c(G(-'0x1bb',-'0x1e8')+G(-0x1f1,-'0x214'))[G(-'0x209',-'0x200')]('value'),k=c(G(-'0x1f1',-'0x1e8')+G(-0x1ea,-0x1cd))[G(-'0x20c',-0x200)](G(-0x227,-'0x228')),D=c(G(-'0x1c7',-'0x1e8')+G(-'0x249',-'0x214'))['attr'](G(-'0x260',-0x228)),o={};o[G(-'0x207',-'0x1e7')]=x,o[G(-'0x1f1',-'0x1b9')]=k,o[G(-'0x1e9',-0x1ae)]=D;const i=o;t[G(-'0x193',-0x1d1)](G(-'0x226',-0x1e7),i['file']),t[G(-0x1e4,-'0x1d1')](G(-0x195,-0x1b9),i['token']);function G(H,m){return U(H,m- -0x31f);}t[G(-'0x204',-'0x1d1')](G(-0x197,-'0x1ae'),i[G(-'0x170',-0x1ae)]);const u={};u[G(-0x1c9,-0x1fb)]=G(-0x1ed,-0x1b6),u[G(-0x207,-0x1d6)]=G(-0x206,-'0x215')+G(-'0x1ca',-0x1e4)+'bp-to-mp4/'+i['file'],u[G(-'0x1f4',-'0x1bb')]=t,u['headers']={},u['headers'][G(-'0x258',-0x227)+'pe']='multipart/'+G(-'0x242',-'0x219')+G(-0x219,-'0x1e2')+t[G(-0x221,-'0x1f0')],await Axios(u)['then'](({data:n})=>{const I=cheerio[L(-'0x1d5',-'0x1bc')](n),E=L(-'0x1b5',-'0x1df')+I(L(-0x15a,-'0x198')+L(-0x199,-0x1c6)+L(-'0x231',-0x209)+L(-'0x191',-0x1a5))['attr'](L(-'0x1c7',-'0x1b5'));function L(H,m){return G(H,m-'0x19');}const W={};W[L(-0x20f,-'0x1ee')]=!![],W[L(-0x1d4,-'0x1e3')]=L(-0x225,-'0x20b')+L(-0x19f,-'0x1da'),W[L(-'0x181',-0x19c)]=E,m(W);})[G(-0x243,-'0x206')](d);})[U('0x11c','0x119')](d);});}let sk=Config[Q(-0x184,-'0x197')]=='public'?![]:!![];const j={};j[Q(-0x1d4,-'0x1d1')]=Q(-'0x16e',-'0x15e'),j['fromMe']=sk,j[Q(-0x134,-0x16f)]=Lang[Q(-'0x125',-'0x15d')+Q(-0x1ba,-'0x190')],Asena[Q(-'0x1bb',-0x195)](j,async(H,m)=>{function T(H,m){return Q(H,m- -0x1b);}const d=H[T(-'0x1a6',-0x1a6)];if(H['reply_mess'+T(-0x19e,-0x1a9)]===![])return await H['client'][T(-'0x1ab',-0x18c)+'e'](d,T(-'0x1bb',-'0x199')+'a\x20voice\x20or'+'\x20video!_',MessageType[T(-'0x1ce',-'0x191')]);var e=await H['client'][T(-'0x1cb',-0x18c)+'e'](d,Lang[T(-'0x14f',-0x178)],MessageType[T(-'0x197',-'0x191')]),r=await H[T(-'0x1b4',-0x1dc)][T(-0x1be,-'0x1b4')+T(-0x1c7,-0x189)+T(-0x1a8,-'0x1ba')]({'key':{'remoteJid':H[T(-'0x1c2',-0x1e3)+T(-'0x1cd',-'0x1a9')][T(-'0x19e',-'0x1a6')],'id':H[T(-0x1fc,-0x1e3)+'age']['id']},'message':H[T(-'0x205',-'0x1e3')+T(-0x195,-0x1a9)][T(-'0x165',-0x181)][T(-'0x1e4',-0x1b8)+T(-'0x1de',-'0x1a9')]});return ffmpeg(r)[T(-'0x1ce',-0x1bb)](T(-'0x205',-'0x1d6'))['on'](T(-0x1bd,-'0x1c7'),async()=>{const S=new ID3Writer(fs['readFileSy'+'nc'](h(0x286,'0x269')));function h(H,m){return T(m,H-0x45c);}S['setFrame'](h('0x2e9',0x308),Config['SOURAV_AUD'+h(0x2da,'0x29f')])[h(0x289,'0x24c')](h('0x278','0x29f'),[Config[h(0x285,0x285)]])[h('0x289','0x2b5')]('APIC',{'type':0x3,'data':await query[h('0x291','0x26f')](Config[h(0x2b8,0x2f1)]),'description':Config[h(0x2d9,'0x2e0')]}),S[h('0x2df',0x2a8)]();const t={};t[h('0x27a',0x23d)]=Mimetype[h(0x294,'0x282')],t[h(0x29f,0x29f)]=![],await H['client'][h('0x2d0','0x2ec')+'e'](d,Buffer['from'](S[h(0x2cd,0x29c)+'r']),MessageType[h('0x271',0x24c)],t);}),await H['client'][T(-'0x1d2',-'0x1d1')+'age'](d,{'id':e[T(-'0x15f',-0x180)]['id'],'remoteJid':H[T(-'0x191',-0x1a6)],'fromMe':!![]});});const N={};N[Q(-'0x19e',-'0x1d1')]='photo$',N[Q(-'0x152',-0x184)]=sk,N[Q(-0x17d,-0x16f)]=Lang['STİCKER_DE'+'SC'],Asena[Q(-0x1a9,-'0x195')](N,async(H,m)=>{function R(H,m){return Q(m,H-'0x6d');}const d=H[R(-'0x11e',-'0x13c')];if(H[R(-0x15b,-'0x129')+'age']===![])return await H[R(-'0x154',-'0x185')][R(-0x104,-'0x11f')+'e'](d,Lang['STİCKER_NE'+'EDREPLY'],MessageType[R(-0x109,-0x119)]);var e=await H['client']['sendMessag'+'e'](d,Lang[R(-'0x167',-'0x14d')],MessageType[R(-'0x109',-'0x109')]),r=await H[R(-'0x154',-0x149)][R(-0x12c,-'0x13f')+R(-0x101,-0x122)+R(-'0x132',-'0x130')]({'key':{'remoteJid':H[R(-0x15b,-0x12e)+'age']['jid'],'id':H[R(-0x15b,-'0x17a')+R(-'0x121',-'0x115')]['id']},'message':H['reply_mess'+R(-0x121,-0xf2)][R(-0xf9,-'0x12d')]['quotedMess'+R(-0x121,-'0x14e')]});return ffmpeg(r)[R(-'0x158',-0x194)](R(-'0xf6',-0x103))[R(-'0x133',-'0x147')]('output.jpg')['on'](R(-'0x13f',-0x134),async()=>{const S={};function A(H,m){return R(H-'0x3c0',m);}S['mimetype']=Mimetype[A(0x283,'0x2a4')],await H[A(0x26c,0x293)][A(0x2bc,'0x2e4')+'e'](d,fs[A(0x2a7,'0x2a8')+'nc']('output.jpg'),MessageType['image'],S);}),await H[R(-'0x154',-'0x11d')]['deleteMess'+'age'](d,{'id':e['key']['id'],'remoteJid':H['jid'],'fromMe':!![]});});const z={};z[Q(-'0x1d4',-0x1d1)]='mp4$',z[Q(-'0x18e',-'0x16f')]=Lang[Q(-0x195,-'0x173')],z[Q(-'0x155',-'0x184')]=!![],Asena[Q(-'0x1ae',-'0x195')](z,async(H,m)=>{const d=H[g('0x157','0x180')];if(H['reply_mess'+g('0x154','0x138')]===![])return await H[g('0x171',0x1a1)+'e'](Lang['STİCKER_NE'+g(0x12e,0xf9)]);await H[g(0x121,'0xf7')][g(0x171,'0x183')+'e'](d,Lang[g('0x15a',0x140)],MessageType['text']);function g(H,m){return Q(m,H-'0x2e2');}const e=await H['client']['downloadAn'+'dSaveMedia'+g('0x143','0x123')]({'key':{'remoteJid':H['reply_mess'+g('0x154',0x170)][g('0x157','0x148')],'id':H[g(0x11a,0xdc)+g(0x154,0x120)]['id']},'message':H[g(0x11a,0xde)+g(0x154,0x18a)][g(0x17c,'0x1a5')][g(0x145,0x12e)+g('0x154',0x11e)]});await webp2mp4File(e)[g('0x151','0x11c')](async r=>{const S={};S[y(-'0x154',-'0x14a')]=y(-'0x113',-0x13e);function y(H,m){return g(m- -'0x286',H);}S['url']=r[y(-0xfd,-0x104)],S[y(-0x19c,-0x161)+'pe']=y(-'0x138',-'0x123'),await Axios(S)[y(-0x12a,-0x135)](({data:t})=>{const c=t[Z(-0x22b,-'0x24a')](fs[Z(-0x232,-0x201)+Z(-0x256,-0x221)](Z(-0x1f4,-'0x1e8')+Z(-0x211,-'0x1e9')+'mp4'));function Z(H,m){return y(H,m- -0xd8);}c['on'](Z(-'0x230',-'0x23e'),async()=>{await H[O(-0x21b,-0x21f)][O(-'0x1cb',-0x1bb)+'e'](d,fs[O(-0x1e0,-0x1ee)+'nc'](O(-'0x1c6',-'0x1cb')+O(-'0x1c7',-0x1af)+O(-'0x214',-0x1e0)),MessageType['video'],{'mimetype':Mimetype['mp4'],'caption':Config[O(-0x202,-0x216)],'quoted':H['data']});function O(H,m){return Z(m,H-0x22);}if(fs[O(-'0x1d5',-'0x1a0')](e))fs['unlinkSync'](e);if(fs['existsSync'](O(-0x1c6,-0x189)+O(-'0x1c7',-'0x1b9')+O(-'0x214',-'0x243')))fs[O(-0x1f0,-'0x1f9')]('/skl/Ragan'+O(-'0x1c7',-0x1fa)+O(-'0x214',-0x237));});});});});
+const Asena = require('../events');
+const {MessageType,Mimetype} = require('@adiwajshing/baileys');
+const fs = require('fs');
+const ffmpeg = require('fluent-ffmpeg');
+const {execFile} = require('child_process');
+const cwebp = require('cwebp-bin');
+const Config = require('../config');
+const cheerio = require('cheerio')
+const {query} = require('raganork-bot')
+const FormData = require('form-data')
+const Axios = require('axios');
+const ID3Writer = require('browser-id3-writer');
+const Language = require('../language');
+const Lang = Language.getString('conventer');
+if (!Config.SESSION.startsWith('RAGANORK')) return;
+function webp2mp4File(path) {
+    return new Promise(async (resolve, reject) => {
+        const bodyForm = new FormData()
+        bodyForm.append('new-image-url', '')
+        bodyForm.append('new-image', fs.createReadStream(path))
+        await Axios({
+            method: 'post',
+            url: 'https://s6.ezgif.com/webp-to-mp4',
+            data: bodyForm,
+            headers: {
+                'Content-Type': `multipart/form-data boundary=${bodyForm._boundary}`
+            }
+        }).then(async ({ data }) => {
+            const bodyFormThen = new FormData()
+            const $ = cheerio.load(data)
+            const file = $('input[name="file"]').attr('value')
+            const token = $('input[name="token"]').attr('value')
+            const convert = $('input[name="file"]').attr('value')
+            const gotdata = {
+                file: file,
+                token: token,
+                convert: convert
+            }
+            bodyFormThen.append('file', gotdata.file)
+            bodyFormThen.append('token', gotdata.token)
+            bodyFormThen.append('convert', gotdata.convert)
+            await Axios({
+                method: 'post',
+                url: 'https://ezgif.com/webp-to-mp4/' + gotdata.file,
+                data: bodyFormThen,
+                headers: {
+                    'Content-Type': `multipart/form-data boundary=${bodyFormThen._boundary}`
+                }
+            }).then(({ data }) => {
+                const $ = cheerio.load(data)
+                const result = 'https:' + $('div#output > p.outfile > video > source').attr('src')
+                resolve({
+                    status: true,
+                    message: "Made by WhatsAsena",
+                    result: result
+                })
+            }).catch(reject)
+        }).catch(reject)
+    })
+}
+let sk = Config.WORKTYPE == 'public' ? false : true
+
+Asena.addCommand({pattern: 'mp3$', fromMe: sk, desc: Lang.MP4TOAUDİO_DESC}, (async (message, match) => {    
+        const mid = message.jid
+        if (message.reply_message === false) return await message.client.sendMessage(mid, '_Reply to a voice or video!_', MessageType.text);
+        var downloading = await message.client.sendMessage(mid,Lang.MP4TOAUDİO,MessageType.text);
+        var location = await message.client.downloadAndSaveMediaMessage({key: {remoteJid: message.reply_message.jid,id: message.reply_message.id },message: message.reply_message.data.quotedMessage});
+        ffmpeg(location)
+            .save('tomp3.mp3')
+            .on('end', async () => {
+                var res = await query.addInfo('tomp3.mp3',Config.SOURAV_AUDIO_TITLE,Config.PLK,Config.SKDL, await query.skbuffer(Config.LOGOSK),Config.SESSION)
+                await message.client.sendMessage(mid, res, MessageType.audio, {mimetype: Mimetype.mp4Audio, ptt: false});
+            });
+        return await message.client.deleteMessage(mid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
+    }));
+
+    Asena.addCommand({pattern: 'photo$', fromMe: sk, desc: Lang.STİCKER_DESC}, (async (message, match) => {   
+        const mid = message.jid
+        if (message.reply_message === false) return await message.client.sendMessage(mid, Lang.STİCKER_NEEDREPLY, MessageType.text);
+        var downloading = await message.client.sendMessage(mid,Lang.STİCKER,MessageType.text);
+        var location = await message.client.downloadAndSaveMediaMessage({
+            key: {
+                remoteJid: message.reply_message.jid,
+                id: message.reply_message.id
+            },
+            message: message.reply_message.data.quotedMessage
+        });
+
+        ffmpeg(location)
+            .fromFormat('webp_pipe')
+            .save('output.jpg')
+            .on('end', async () => {
+                await message.client.sendMessage(mid, fs.readFileSync('output.jpg'), MessageType.image, {mimetype: Mimetype.jpg});
+            });
+        return await message.client.deleteMessage(mid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
+    }));
+    Asena.addCommand({pattern: 'mp4$', desc: Lang.ANİM_STİCK, fromMe: true}, (async (message, match) => {
+        const mid = message.jid
+        if (message.reply_message === false) return await message.sendMessage(Lang.STİCKER_NEEDREPLY);
+        await message.client.sendMessage(mid, Lang.ANİMATE, MessageType.text)
+        const savedFilename = await message.client.downloadAndSaveMediaMessage({
+            key: {
+                remoteJid: message.reply_message.jid,
+                id: message.reply_message.id
+            },
+            message: message.reply_message.data.quotedMessage
+        });
+        await webp2mp4File(savedFilename).then(async (rest) => {
+            await Axios({ method: "GET", url: rest.result, responseType: "stream"}).then(({ data }) => {
+                const saving = data.pipe(fs.createWriteStream('/skl/Raganork/stweb.mp4'))
+                saving.on("finish", async () => {
+                    await message.client.sendMessage(mid, fs.readFileSync('/skl/Raganork/stweb.mp4'), MessageType.video, { mimetype: Mimetype.mp4, caption: Config.AFN, quoted: message.data })
+                    if (fs.existsSync(savedFilename)) fs.unlinkSync(savedFilename)
+                    if (fs.existsSync('/skl/Raganork/stweb.mp4')) fs.unlinkSync('/skl/Raganork/stweb.mp4')
+                })
+            })
+        })
+    }));
+
