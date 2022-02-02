5b5e82f232443ee2b0648981e044aefb2d033ecc
Sourav K <87891508+souravkl11@users.noreply.github.com>
2022-02-02 22:00:16 +0530

    Fixed video temporarily

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -234,8 +234,8 @@ skl.addCommand({pattern: 'video ?(.*)', fromMe: sourav, desc: Lang.VIDEO_DESC}, 
         /(?:http(?:s|):\/\/|)(?:(?:www\.|)youtube(?:\-nocookie|)\.com\/(?:watch\?.*(?:|\&)v=|embed|shorts\/|v\/)|youtu\.be\/)([-_0-9A-Za-z]{11})/
         var qq = getID.exec(s1)
         await message.client.sendMessage(message.jid,Lang.DOWNLOADING_VIDEO,MessageType.text, {quoted : { key: {fromMe: true,participant: "0@s.whatsapp.net",remoteJid: "status@broadcast"},message: {"extendedTextMessage": {"text": config.BOTSK }}}});
-      var dl = await get.query.getVideo(qq[0],v)
-var cap = dl.details.title != 'error' ? dl.details.title : s.AFN
+     // var dl = await get.query.getVideo(qq[0],v)
+var cap = /*dl.details.title != 'error' ? dl.details.title : */s.AFN
 var yt = ytdl(qq[1], {filter: format => format.container === 'mp4' && ['720p', '480p', '360p', '240p', '144p'].map(() => true)});
         yt.pipe(fs.createWriteStream('./' + qq[1] + '.mp4'));
         yt.on('end', async () => {
