373d223e9e16a9e4f169092f76cc9a0c74c5507a
Sourav K <87891508+souravkl11@users.noreply.github.com>
2022-02-08 22:13:57 +0530

    Fix test

--- Raganork/Message.js
+++ Raganork/Message.js
@@ -1,11 +1,3 @@
-/* Copyright (C) 2020 Yusuf Usta.
-
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-
-WhatsAsena - Yusuf Usta
-*/
-
 const {MessageType, Presence, MessageOptions} = require('@adiwajshing/baileys');
 const Base = require('./Base');
 const ReplyMessage = require('./ReplyMessage');
@@ -15,37 +7,31 @@ class Message extends Base {
         super(client);
         if (data) this._patch(data);
     }
-    
+
     _patch(data) {
         this.id = data.key.id === undefined ? undefined : data.key.id;
         this.jid = data.key.remoteJid;
         this.fromMe = data.key.fromMe;
-        this.message = data && data.message && data.message.extendedTextMessage ?  data.message.extendedTextMessage.text : /*data.message.conversation*/ '';
+        this.message = data.message.extendedTextMessage === null ? data.message.conversation : data.message.extendedTextMessage.text;
         this.unreadCount = data.unreadCount;
         this.timestamp = typeof(data.messageTimestamp) === 'object' ? data.messageTimestamp.low : data.messageTimestamp;
         this.data = data;
-        
-        if (data.message && data.message.hasOwnProperty('extendedTextMessage') &&
+
+        if (data.message.hasOwnProperty('extendedTextMessage') &&
                 data.message.extendedTextMessage.hasOwnProperty('contextInfo') === true && 
                 data.message.extendedTextMessage.contextInfo.hasOwnProperty('quotedMessage')) { 
             this.reply_message = new ReplyMessage(this.client, data.message.extendedTextMessage.contextInfo); } else {
                 this.reply_message = false;
             }
-       /* if (data.message.hasOwnProperty('buttonResponseMessage') &&
-                data.message.extendedTextMessage.hasOwnProperty('contextInfo') === true && 
-                data.message.extendedTextMessage.contextInfo.hasOwnProperty('quotedMessage')) { 
-            this.reply_message = new ReplyMessage(this.client, data.message.extendedTextMessage.contextInfo); } else {
-                this.reply_message = false;
-            }*/
-        
-        if (data.message && data.message.hasOwnProperty('extendedTextMessage') &&
+
+        if (data.message.hasOwnProperty('extendedTextMessage') &&
         data.message.extendedTextMessage.hasOwnProperty('contextInfo') === true && 
         data.message.extendedTextMessage.contextInfo.hasOwnProperty('mentionedJid')) {
             this.mention = data.message.extendedTextMessage.contextInfo.mentionedJid;
         } else {
             this.mention = false;
         }
-        
+
         return super._patch(data);
     }
 
@@ -71,4 +57,4 @@ class Message extends Base {
     }
 };
 
-module.exports = Message;
+module.exports = Message; 
