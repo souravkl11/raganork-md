9cd57ed420b63ad42cc000eb56cd6f001ec0b70e
Sourav K <87891508+souravkl11@users.noreply.github.com>
2022-02-10 19:55:54 +0530

    Add new key FIND_KEY. Get API Token from audd.io

--- plugins/music_finder.js
+++ plugins/music_finder.js
@@ -2,20 +2,16 @@ var e = require('../events')
 var {MessageType,Mimetype} = require('@adiwajshing/baileys')
 const c = require('../config')
 var f = require('raganork-bot')
-var v = c.CHANNEL
-e.addCommand({pattern: 'find ?(.*)', fromMe: true}, (async (m, match) => {    
+var v = c.SESSION
+var fm = c.WORKTYPE == 'public' ? false : true
+e.addCommand({pattern: 'find ?(.*)', fromMe: fm}, (async (m, match) => {    
 if (!m.reply_message.text && !m.reply_message.video && !m.reply_message.sticker && !m.reply_message.image) {
 var q = await m.client.downloadAndSaveMediaMessage({key: { remoteJid: m.reply_message.jid,id: m.reply_message.id}, message: m.reply_message.data.quotedMessage});
-var k = c.ZEKAIS_API.split(',')
-var k1 = k[Math.floor(Math.random()*k.length)];	
-var r = await f.query.music(q,k1,v)
+var k = c.find_key
+var r = await f.query.music(q,k,v)
 if (r.result) {
 let msg =  '_Title:_ *' + r.result.title + '* \n_Album:_ *' + r.result.album+'* \n_Artist:_ *' + r.result.artist+ '* \n_Label:_ *' + r.result.label+'* \n_Release date:_ *' + r.result.release_date + '* \n_Song link:_ ' + 'https://www.youtube.com/results?search_query='+r.result.title.split(' ').join('+')
 return await m.client.sendMessage(m.jid, msg, MessageType.text, {quoted: m.data})}
-if (r.error) return await m.client.sendMessage(m.jid, "_Error " + r.error.error_code + '_', MessageType.text,{quoted: m.data})
-if (!r.result) return await m.client.sendMessage(m.jid, "_Failed!_", MessageType.text,{quoted: m.data})}
+if (r.error && r.error.error_code == '900') return await m.client.sendMessage(m.jid, "_API Key expired! Get new api key_", MessageType.text,{quoted: m.data})
+if (!r.result) return await m.client.sendMessage(m.jid, "_Not found!_", MessageType.text,{quoted: m.data})}
 else {return await m.client.sendMessage(m.jid, "_Reply to any music!_", MessageType.text,{quoted: m.data})}}));
-if (c.WORKTYPE === 'public') {
-e.addCommand({pattern: 'find ?(.*)', fromMe: false}, (async (m, match) => {    
-return await m.client.sendMessage(m.jid, "_Only the owner can use this command!_", MessageType.text,{quoted: m.data})
-}));}
