f38f59c304393019b8ab007af87a743532d9d300
Sourav K <87891508+souravkl11@users.noreply.github.com>
2022-02-11 09:25:21 +0530

    .take now supports audio (test stage)

--- plugins/sticker_exif.js
+++ plugins/sticker_exif.js
@@ -11,7 +11,11 @@ const h = require('heroku-client');
 const he = new h({token: w.HEROKU.API_KEY});
 let ur = '/apps/' + w.HEROKU.APP_NAME;
 e.addCommand({pattern: 'take ?(.*)', fromMe: a, desc:'Changes sticker pack & author name!'}, (async (m, match) => { 
+if (!m.reply_message.data.quotedMessage) return await m.sendMessage('_Reply to an audio or a sticker_')
+var audiomsg = m.reply_message.data.quotedMessage.audioMessage;
+var stickermsg = m.reply_message.data.quotedMessage.stickerMessage;
 var q = await m.client.downloadAndSaveMediaMessage({key: {remoteJid: m.reply_message.jid,id: m.reply_message.id},message: m.reply_message.data.quotedMessage});
+if (stickermsg) {
 var s = w.SOURAVKL11.split('|');
 var au = s[1];
 var p = s[0];
@@ -20,7 +24,20 @@ ffmpeg(q)
 .save('ex.png')
 .on('end', async () => {
 var res = await i.query.sticker('ex.png',au,p,v)
-await m.client.sendMessage(m.jid, await i.query.skbuffer(res),MessageType.sticker,{quoted:m.data});})}));
+await m.client.sendMessage(m.jid, await i.query.skbuffer(res),MessageType.sticker,{quoted:m.data});})
+} 
+else if (audiomsg) {
+ffmpeg(q)
+.save('info.mp3')
+.on('end', async () => {
+var spl = w.AUDIO_DATA.split(';')
+        var res = await i.query.addInfo('info.mp3',spl[0],spl[1],'Raganork Engine', await i.query.skbuffer(spl[2]),w.SESSION)
+        await m.client.sendMessage(m.jid, res, MessageType.audio, {quoted:m.data,mimetype: Mimetype.mp4Audio, ptt: false});
+    });
+    
+}
+else return await m.sendMessage('_Reply to an audio or a sticker_')
+}));
 e.addCommand({pattern: 'wm ?(.*)', fromMe: a, desc:'Sets sticker pack & author name with given ones.'}, (async (m, t) => { 
 var q = await m.client.downloadAndSaveMediaMessage({key: {remoteJid: m.reply_message.jid,id: m.reply_message.id},message: m.reply_message.data.quotedMessage});
 var au,p;
@@ -52,4 +69,8 @@ e.addCommand({ pattern: 'setexif ?(.*)', fromMe: true}, (async (m, qu) => {
 if (!qu[1]) {return await m.client.sendMessage(m.jid,'_Need some data \n Example: \n .setexif Name|Author_',MessageType.text,{quoted:m.data})}
 await m.client.sendMessage(m.jid, '_Added new exif!_',MessageType.text,{quoted:m.data});
 await he.patch(ur + '/config-vars', { body: {['STICKER_DATA']: qu[1]}});}));
-    
+e.addCommand({ pattern: 'audinfo ?(.*)', fromMe: true}, (async (m, qu) => {
+if (!qu[1]) {return await m.client.sendMessage(m.jid,'_Need some data like: *.audinfo Title;Artist;Imagelink*_',MessageType.text,{quoted:m.data})}
+await m.client.sendMessage(m.jid, '_Added new audio info!_',MessageType.text,{quoted:m.data});
+await he.patch(ur + '/config-vars', { body: {['AUDIO_DATA']: qu[1]}});}));
+        
