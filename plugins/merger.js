54fc5a03410610414a101b0edf52f94fc3e7b93c
Sourav K <87891508+souravkl11@users.noreply.github.com>
2022-02-15 18:50:38 +0530

    fix

--- plugins/merger.js
+++ plugins/merger.js
@@ -12,15 +12,15 @@ var vm = m.reply_message.data.quotedMessage.videoMessage;
 var q = await m.client.downloadMediaMessage({key: { remoteJid: m.reply_message.jid,id: m.reply_message.id}, message: m.reply_message.data.quotedMessage});
 if (!rm) return await m.client.sendMessage(m.jid, '_Reply to an audio or video!_', MessageType.text, {quoted: m.data})
 if (!am && !vm) return await m.client.sendMessage(m.jid, '_Reply to an audio or video!_', MessageType.text, {quoted: m.data})
-if (rm && am && !fs.existsSync('aud.mp3')) {
-await fs.writeFileSync('aud.mp3',q)
+if (rm && am && !fs.existsSync('aud.mpeg')) {
+await fs.writeFileSync('aud.mpeg',q)
 await m.client.sendMessage(m.jid, '_Saved audio! Next, reply to the video to be mixed_', MessageType.text, {quoted: m.data})}
 if (rm && vm && !fs.existsSync('vid.mp4')) {
 await fs.writeFileSync('vid.mp4',q)
 await m.client.sendMessage(m.jid, '_Saved video! Next, reply to the audio to be mixed_', MessageType.text, {quoted: m.data})}
-if (rm && vm && fs.existsSync('aud.mp3') && fs.existsSync('vid.mp4')) {
-query.AVmix('aud.mp3','vid.mp4','AV_mix.mp4',v, async function(video) {
-await m.client.sendMessage(m.jid, video, MessageType.video, { mimetype: Mimetype.mp4, quoted: m.data});
+if (rm && vm && fs.existsSync('aud.mpeg') && fs.existsSync('vid.mp4')) {
+query.AVmix('aud.mpeg','vid.mp4','AV_mix.mp4',v, async function(video) {
+return await m.client.sendMessage(m.jid, video, MessageType.video, { mimetype: Mimetype.mp4, quoted: m.data});
 });    
 }
 }));
