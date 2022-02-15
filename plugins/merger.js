b8742d8b63b3a793a997927ddf37912b4af4eb53
Sourav K <87891508+souravkl11@users.noreply.github.com>
2022-02-15 20:45:23 +0530

    Fixed audio mixer();

--- plugins/merger.js
+++ plugins/merger.js
@@ -22,7 +22,7 @@ ffmpeg(q)
 await m.client.sendMessage(m.jid, '_Saved audio! Next, reply to the video to be mixed_', MessageType.text, {quoted: m.data})})}
 if (rm && vm && !fs.existsSync('vid.mp4')) {
 await fs.writeFileSync('vid.mp4',qb)
-await m.client.sendMessage(m.jid, '_Saved video! Next, reply to the audio to be mixed_', MessageType.text, {quoted: m.data})}
+await m.client.sendMessage(m.jid, '_Saved video! Next, Processing.._', MessageType.text, {quoted: m.data})}
 if (rm && vm && fs.existsSync('audio_v.mp3') && fs.existsSync('vid.mp4')) {
 query.AVmix('vid.mp4','audio_v.mp3','AV_mix.mp4',v, async function(video) {
 return await m.client.sendMessage(m.jid, video, MessageType.video, { mimetype: Mimetype.mp4, quoted: m.data});
@@ -44,9 +44,9 @@ await m.client.sendMessage(m.jid, '_Saved audio 1_', MessageType.text, {quoted: 
 if (rm && am && !fs.existsSync('audio_2.mp3')) {
 ffmpeg(q)
 .format('mp3')
-.save('audio_1.mp3')
+.save('audio_2.mp3')
 .on('end', async () => {
-await m.client.sendMessage(m.jid, '_Saved video! Next, reply to the audio to be mixed_', MessageType.text, {quoted: m.data})})}
+await m.client.sendMessage(m.jid, '_Saved audio 2. Processing..._', MessageType.text, {quoted: m.data})})}
 if (rm && vm && fs.existsSync('audio_1.mp3') && fs.existsSync('audio_2.mp3')) {
 query.MixAudio('audio_1.mp3','audio_2.mp3','amix.mp3',v, async function(audio) {
 return await m.client.sendMessage(m.jid, audio, MessageType.audio, { mimetype: Mimetype.mp3, quoted: m.data});
