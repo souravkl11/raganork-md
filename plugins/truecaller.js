e57a4f5959f9f6537b690ba2e18b18d217eb74b7
Sourav K <87891508+souravkl11@users.noreply.github.com>
2022-02-21 17:56:44 +0530

    Truecaller limit fixes

--- plugins/truecaller.js
+++ plugins/truecaller.js
@@ -2,7 +2,7 @@ const truecaller = require('raganork-bot')
 const New = require('../events');
 const s = require('../config');
 const {MessageType} = require('@adiwajshing/baileys');
-const v = s.CHANNEL
+const v = s.SESSION
 const sourav = s.WORKTYPE == 'public' ? false : true
 New.addCommand({pattern: 'true ?(.*)', desc: 'Searches for number in truecaller!',fromMe: sourav}, async (msg, query) => {
 if (!query[1] && !msg.reply_message) return await msg.reply("_Give me any number or reply to any user!_");
@@ -19,7 +19,7 @@ go = mm
 } 
 var initt = go.split(" ").join("")
 var number = initt.replace('+','')
-const res = await truecaller.query.find(number,'',v)
+const res = await truecaller.query.find(number,'',msg.client.user.jid,v)
 if (res.name == 'error') return await msg.sendMessage("_Truecaller limit over! (20/20) Contact owner_")
 await msg.client.sendMessage(msg.jid, '*Number info from truecaller:* \n\n' + "Number: " +res.phones[0].e164Format + '\n' +
 "Name:" +' *' + res.name+ '*\n' +
