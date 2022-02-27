b7fa58f8b4d875f85059818d69c5e945a156a72e
Sourav K <87891508+souravkl11@users.noreply.github.com>
2022-02-27 14:26:43 +0530

    Fixed and added reset warn beta();

--- plugins/warn.js
+++ plugins/warn.js
@@ -17,12 +17,21 @@ var reason = mat[1] ? mat[1] : 'Replied message'
 var msg = "```Warning ⚠️```"+ '\n' +
 "User: " +'@'+par.split('@')[0] + '\n' +
 "Reason:" +' *' + reason+ '*\n' +
-"Remaining:" +' *' + warn + '*\n' +
-"Career:" +' *' + res.phones[0].carrier + '*\n'
+"Remaining:" +' *' + warn + '*\n' 
 if (warn !== 0) {
     return await m.client.sendMessage(chat,msg,MessageType.text,{quoted:m.data,contextInfo: {mentionedJid: par}})
 } else {
-    await m.client.sendMessage(chat,'_Warn limit() exceeded. Removing @'+par.split('@')[0]+ '_',MessageType.text,{quoted:m.data,contextInfo: {mentionedJid: par}})
+    await m.client.sendMessage(chat,'_Warn limit('+cnt+') exceeded. Removing @'+par.split('@')[0]+ '_',MessageType.text,{quoted:m.data,contextInfo: {mentionedJid: par}})
     await m.client.groupRemove(m.jid, [m.reply_message.data.participant]);
 }
 }));
+e.addCommand({pattern: 'reset warn', fromMe: true, desc:'Warns user. Removes user after maximum number of warns'}, (async (m, mat) => { 
+    if (!m.reply_message) return await m.sendMessage('Reply to any message!_')
+        var par = m.reply_message.jid
+    var me = m.client.user.jid.split('@')[0]
+    var chat = m.jid
+    if (!chat.endsWith('@g.us')) return await m.sendMessage('_Only works in groups!_')
+    await query.deletewarn(me,chat,par,v)
+    await m.client.sendMessage(chat,'_Successfully reset warn limits ('+cnt+') of @'+par.split('@')[0]+ '_',MessageType.text,{quoted:m.data,contextInfo: {mentionedJid: par}})    
+}));
+    
