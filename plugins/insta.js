fdc61262b24b21e9a1378c64e95250d29d40e6f9
Sourav K <87891508+souravkl11@users.noreply.github.com>
2022-03-01 10:36:40 +0530

    Added .insta, .story and .ig to command list and fixed multiple story msgs

--- plugins/insta.js
+++ plugins/insta.js
@@ -18,7 +18,7 @@ var need_acc = "*_Need an instagram username!_*";
 var fail = "*_Download failed! Check your link and try again_*";
 var need_acc_s = "_Need an instagram username or link!_";
 let sourav = setting.WORKTYPE == 'public' ? false : true
-skl.addCommand({ pattern: 'insta ?(.*)', fromMe: sourav,dontAddCommandList: true }, (async (msg, query) => {
+skl.addCommand({ pattern: 'insta ?(.*)', fromMe: sourav, desc:'Downloads post/reel/igtv from instagram',usage:'insta link or reply to a link'}, (async (msg, query) => {
 var q = !msg.reply_message.message ? query[1] : msg.reply_message.message
 if (!q)  return await msg.client.sendMessage(msg.jid, '_Unable to read link from message!_', MessageType.text, {quoted: msg.data});
 if (q && !q.includes('instagram.com')) return await msg.client.sendMessage(msg.jid, need, MessageType.text, {quoted: msg.data});
@@ -48,20 +48,21 @@ if (res.links[0].url.includes('jpg')) return await msg.client.sendMessage(msg.ji
     
 }
 }));
-skl.addCommand({ pattern: 'ig ?(.*)', fromMe: sourav,dontAddCommandList: true }, (async (msg, query) => {
+skl.addCommand({ pattern: 'ig ?(.*)', fromMe: sourav, desc:'Gets account info from instagram',usage:'ig username'}, (async (msg, query) => {
     if (query[1] === '') return await msg.client.sendMessage(msg.jid, need_acc, MessageType.text, {quoted: msg.data});
     var res = await raganork.query.getStalk(query[1])
     if (res === "false") return await msg.client.sendMessage(msg.jid, "_Username invalid!_", MessageType.text, {quoted: msg.data})
     var buffer = await raganork.query.skbuffer(res.hd_profile_pic_url_info.url)
     await msg.client.sendMessage(msg.jid, buffer, MessageType.image, { mimetype: Mimetype.jpg, caption: '_*Name:*_ ' + `${res.fullname}` + '\n _*Bio:*_ ' + `${res.biography}`+ '\n _*Private account:*_ ' + `${res.is_private} ` + '\n _*Followers:*_ ' + `${res.followers}` + '\n _*Following:*_ ' + `${res.following}` + '\n _*Posts:*_ ' + `${res.post_count}` + '\n _*Verified:*_ ' + `${res.is_verified} ` + '\n _*IGTV videos:*_ ' + `${res.total_igtv_videos}`, quoted: msg.data});
     }));
-skl.addCommand({ pattern: 'story ?(.*)', fromMe: sourav,dontAddCommandList: true }, (async (msg, query) => {
+skl.addCommand({ pattern: 'story ?(.*)', fromMe: sourav, desc:'Downloads full/single story from instagram',usage:'.story username or link'}, (async (msg, query) => {
 if (query[1] === '') return await msg.client.sendMessage(msg.jid, need_acc_s, MessageType.text, {quoted: msg.data});
 var user = query[1];
 var res = await raganork.query.getStory(user,v)
 if (res === "false") return await msg.client.sendMessage(msg.jid, "_Story not found!_", MessageType.text, {quoted: msg.data})
 if (res.error) return await msg.client.sendMessage(msg.jid, res.error.replace('status','story'), MessageType.text, {quoted: msg.data})
 var url = ''
+await msg.sendMessage('```Downloading '+res.result.stories.length+' stories of '+res.result.username+'```');
 res.result.stories.map((result) => {
 url += result.url + ','});
 var que = url !== false ? url.split(',') : [];
@@ -71,7 +72,6 @@ var type = que[i].includes('mp4') ? MessageType.video : MessageType.image
 var mime = que[i].includes('mp4') ? Mimetype.mp4 : Mimetype.jpg
 var stream = get.buffer();
 stream.then(async (video) => {
-await msg.sendMessage('```Downloading '+res.result.stories_count+' stories of '+res.result.username+'```');
 await msg.client.sendMessage(msg.jid, video, type, { mimetype: mime,quoted: msg.data});
 })};
 }));
