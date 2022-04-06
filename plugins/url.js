7ee31f94661854a5120b534a31a73222d2e05d42
Sourav K <87891508+souravkl11@users.noreply.github.com>
2022-04-06 16:34:58 +0530

    Fix url

--- plugins/url.js
+++ plugins/url.js
@@ -8,6 +8,7 @@ const v = w.SUPPORT2
 const {upload} = require('raganork-bot');
 let a = w.WORKTYPE == 'public' ? false : true;
 e.addCommand({pattern: 'url ?(.*)', fromMe: a, desc:'Uploads image to imgur.com'}, (async (m, match) => { 
+if (!m.reply_message.image && !m.reply_message.video ) return;
 var q = await m.client.downloadAndSaveMediaMessage({key: {remoteJid: m.reply_message.jid,id: m.reply_message.id},message: m.reply_message.data.quotedMessage});
 var res = await upload(q,v)
 await m.client.sendMessage(m.jid, '_*File uploaded!*_ \n\n_File name:_ ' + res.id + '.jpeg \n_File URL:_ ' + res.link + '\n_File size:_ ' + res.size + ' bits' + '\n_Format:_ ' + res.type + '\n_Height:_ ' + res.height + '\n_Width:_ ' + res.width , MessageType.text, { quoted: m.data });}));
