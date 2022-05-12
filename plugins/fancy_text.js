d954d75c64791a96849e092d6b8b11bb184acbe6
Sourav <87891508+souravkl11@users.noreply.github.com>
2022-05-12 15:44:46 +0530

    Update fancy_text.js

--- plugins/fancy_text.js
+++ plugins/fancy_text.js
@@ -8,7 +8,7 @@ const {addCommand} = require('../events')
 const {Fancy} = require('raganork-bot')
 const {WORKTYPE} = require('../config');
 var w = WORKTYPE == 'public'? false:true
-Module({pattern: 'fancy ?(.*)', fromMe: w, desc: 'Creates fancy text fonts'}, (async (message, match) => {
+addCommand({pattern: 'fancy ?(.*)', fromMe: w, desc: 'Creates fancy text fonts'}, (async (message, match) => {
 if (!match[1]||!message.reply_message.message) return await message.sendReply("Reply to any message with .fancy number\n"+Fancy("example","32"))
 try { var result = Fancy(message.reply_message.message,match[1]) } catch (e) {return await message.sendReply(e.message)}
 await message.reply(result)
