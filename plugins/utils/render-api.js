9781b24acc63fc0f8b36dcc38ffcd6b8f99dbf10
Sourav <87891508+souravkl11@users.noreply.github.com>
2025-06-06 13:50:05 +0530

    feat: disable autoscaling on render

--- plugins/utils/render-api.js
+++ plugins/utils/render-api.js
@@ -1,15 +1,36 @@
+import axios from 'axios';
+import fetch from 'node-fetch';
+
 async function deployLatestCommit(serviceId, apiKey) {
   if (!serviceId) {
     console.error("Error: RENDER_SERVICE_ID is not set.");
-    console.error("Please provide your Render Service ID.");
     return;
   }
 
   if (!apiKey) {
     console.error("Error: RENDER_API_KEY is not set.");
-    console.error("Please provide your Render API Key.");
     return;
   }
+
+  const disableAutoScaling = async () => {
+    const autoScalingUrl = `https://api.render.com/v1/services/${serviceId}/autoscaling`;
+    const headers = {
+      accept: 'application/json',
+      authorization: `Bearer ${apiKey}`
+    };
+
+    try {
+      const res = await axios.delete(autoScalingUrl, { headers });
+      console.log("Autoscaling disabled:", res.data);
+    } catch (err) {
+      console.error("Error disabling autoscaling:", err.message);
+      return false;
+    }
+    return true;
+  };
+
+  const proceed = await disableAutoScaling();
+  if (!proceed) return;
 
   const apiUrl = `https://api.render.com/v1/services/${serviceId}/deploys`;
 
@@ -20,7 +41,7 @@ async function deployLatestCommit(serviceId, apiKey) {
   };
 
   const payload = {
-    "clearCache": "clear" 
+    "clearCache": "clear"
   };
 
   console.log(`Attempting to deploy service: ${serviceId}...`);
@@ -48,11 +69,9 @@ async function deployLatestCommit(serviceId, apiKey) {
       console.log(`Commit Message: ${deployInfo.commit.message}`);
     }
     console.log(`Render Dashboard Link: https://dashboard.render.com/web/${serviceId}/deploys/${deployInfo.id}`);
-
   } catch (error) {
     console.error(`An error occurred during deployment: ${error.message}`);
-
   }
 }
 
-module.exports = deployLatestCommit
+module.exports = deployLatestCommit;
