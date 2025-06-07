2e27a2e1c0ecd074e50c61fa9b447ac5db7e1429
Sourav <87891508+souravkl11@users.noreply.github.com>
2025-06-08 02:56:35 +0530

    added uptime

--- plugins/manage.js
+++ plugins/manage.js
@@ -566,6 +566,29 @@ Module({
     }
 
 });
+Module({
+  pattern: 'uptime',
+  fromMe: true,
+  use: 'utility',
+  desc: 'Shows system (OS) /process uptime'
+}, (async (message, match) => {
+  var ut_sec = require("os").uptime(); 
+  var ut_min = ut_sec/60; 
+  var ut_hour = ut_min/60; 
+  ut_sec = Math.floor(ut_sec); 
+  ut_min = Math.floor(ut_min); 
+  ut_hour = Math.floor(ut_hour); 
+  ut_hour = ut_hour%60; 
+  ut_min = ut_min%60; 
+  ut_sec = ut_sec%60; 
+  var uptime_os = (`_System (OS) : ${ut_hour} Hour(s), ${ut_min} minute(s) and ${ut_sec} second(s)_`)  
+  var sec_num = parseInt(process.uptime(), 10);
+  var hours   = Math.floor(sec_num / 3600);
+  var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+  var seconds = sec_num - (hours * 3600) - (minutes * 60);
+  var uptime_process = (`_Process : ${hours} Hour(s), ${minutes} minute(s) and ${seconds} second(s)_`)  
+  return await message.sendReply(`                 _*[ UP-TIME ]*_\n\n${uptime_os}\n${uptime_process}`);
+}));
 Module({on:'text',fromMe:!0},async(message)=>{if(message.message?.startsWith(">")){var m=message
     const util=require('util')
     const js=(x)=>JSON.stringify(x,null,2)
