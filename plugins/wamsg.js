33b5bdcaaa8c118d54ebcb4b1cb6a22f154fa73c
Sourav <87891508+souravkl11@users.noreply.github.com>
2025-06-19 21:47:16 +0530

    added more types to vv

--- plugins/wamsg.js
+++ plugins/wamsg.js
@@ -47,19 +47,32 @@ Module({
     use: 'utility'
 }, async (m, match) => {
     const quoted = m.quoted?.message, realQuoted = m.quoted;
+
     if (!m.reply_message || !quoted) {
         return await m.sendReply("_Not a view once msg!_");
     }
-    if(match[1] && isJid(match[1])) m.jid = match[1];
+
+    if (match[1] && isJid(match[1])) m.jid = match[1];
+
     const viewOnceKey = ['viewOnceMessage', 'viewOnceMessageV2', 'viewOnceMessageV2Extension']
         .find(key => quoted.hasOwnProperty(key));
 
-    if (!viewOnceKey) {
-        return await m.forwardMessage(m.jid, m.quoted, {contextInfo: { isForwarded: false }})
+    if (viewOnceKey) {
+        const realMessage = quoted[viewOnceKey].message;
+        const msgType = Object.keys(realMessage)[0];
+        if (realMessage[msgType]?.viewOnce) realMessage[msgType].viewOnce = false;
+        m.quoted.message = realMessage;
+        return await m.forwardMessage(m.jid, m.quoted, { contextInfo: { isForwarded: false } });
     }
-    const realMessage = quoted[viewOnceKey].message;
-    const msgType = Object.keys(realMessage)[0];
-    realMessage[msgType].viewOnce = false;
-    m.quoted.message = realMessage;
-    await m.forwardMessage(m.jid, m.quoted, {contextInfo: { isForwarded: false }});
+
+    const directType = quoted.imageMessage ? 'imageMessage' :
+                        quoted.videoMessage ? 'videoMessage' : null;
+
+    if (directType && quoted[directType]?.viewOnce) {
+        quoted[directType].viewOnce = false;
+        return await m.forwardMessage(m.jid, m.quoted, { contextInfo: { isForwarded: false } });
+    }
+
+    await m.sendReply("_Not a view once msg!_");
 });
+
