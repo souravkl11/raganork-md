a8cf2585786f8c83c9d549182636e6c007be90dd
Sourav <87891508+souravkl11@users.noreply.github.com>
2025-07-11 16:53:32 +0530

    fix

--- public-repo/core/constructors/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    Base: require('./base'),
-    Message: require('./message'),
-    ReplyMessage: require('./reply-message'),
-    Image: require('./image'),
-    Video: require('./video'),
-    GroupUpdate: require('./group-update')
-};
